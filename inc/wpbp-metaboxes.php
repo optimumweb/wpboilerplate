e37d593c73ca53b1bd7e932a59417412cfaa199e
Jonathan Roy <jroy66@gmail.com>
2011-08-16 12:31:38 -0400

    Improving custom write panels, aka wpbp-metaboxes

--- inc/wpbp-metaboxes.php
+++ inc/wpbp-metaboxes.php
@@ -48,7 +48,7 @@ function wpbp_display_meta_box( $post, $wpbp_add_meta_box_args )
 	$wpbp_meta_box_nonce_name = WPBP_META_BOX_PREFIX . $wpbp_meta_box_key . '-nonce';
 	$wpbp_meta_box_nonce_value = wp_create_nonce( basename( __FILE__ ) );
 	$wpbp_meta_box_fields = $wpbp_meta_box['fields'];
-	$wpbp_meta_box_data = get_post_meta( $post->ID, $wpbp_meta_box_key, true );
+	$wpbp_meta_box_data = get_post_meta( $post->ID, $wpbp_meta_box_key, false );
 ?>
 <div class="form-wrap">
 	<input type="hidden" name="<?php echo $wpbp_meta_box_nonce_name; ?>" value="<?php echo $wpbp_meta_box_nonce_value; ?>" />
