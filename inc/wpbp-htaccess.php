a430f693edd6224341836ca5a6bf43a0cdcc230b
Jonathan Roy <jroy66@gmail.com>
2011-08-24 12:50:12 -0400

    Add url rewriting rule to resize images

--- inc/wpbp-htaccess.php
+++ inc/wpbp-htaccess.php
@@ -24,11 +24,11 @@ if (stristr($_SERVER['SERVER_SOFTWARE'], 'apache') !== false) {
 		$theme_name = next(explode('/themes/', get_stylesheet_directory()));
 		global $wp_rewrite;
 		$wpbp_new_non_wp_rules = array(
-			'css/(.*)'      => 'wp-content/themes/'. $theme_name . '/css/$1',
-			'js/(.*)'       => 'wp-content/themes/'. $theme_name . '/js/$1',
-			'img/(.*)'      => 'wp-content/themes/'. $theme_name . '/img/$1',
-			'img/resize/'   => 'wp-content/themes/wpboilerplate/img/resize.php',
-			'plugins/(.*)'  => 'wp-content/plugins/$1'
+			'css/(.*)'     => 'wp-content/themes/'. $theme_name . '/css/$1',
+			'js/(.*)'      => 'wp-content/themes/'. $theme_name . '/js/$1',
+			'img/resize/'  => 'wp-content/themes/wpboilerplate/img/resize.php',
+			'img/(.*)'     => 'wp-content/themes/'. $theme_name . '/img/$1',
+			'plugins/(.*)' => 'wp-content/plugins/$1'
 		);
 		$wp_rewrite->non_wp_rules += $wpbp_new_non_wp_rules;
 	}
