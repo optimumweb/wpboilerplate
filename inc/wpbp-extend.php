705fc3dc0d7359d7d04404ef0b9e13d0352df910
Jonathan Roy <jroy66@gmail.com>
2011-08-24 13:14:51 -0400

    Fixing custom post thumbnail 'alt' for get_the_title()

--- inc/wpbp-extend.php
+++ inc/wpbp-extend.php
@@ -30,7 +30,7 @@ if ( !function_exists('wpbp_post_thumbnail') ) {
 	function wpbp_post_thumbnail($post_ID, $width = 150, $height = 150, $quality = 0.9)
 	{
 		$url = get_post_meta( $post_ID, 'featured_image_url', true );
-		$alt = get_title( $post_ID );
+		$alt = get_the_title( $post_ID );
 		$src = get_bloginfo('stylesheet_directory') . '/img/resize.php?w=' . $width . '&h=' . $height . '&q=' . $quality . '&src=' . $url;
 		echo "<img src=\"" . $src . "\" width=\"" . $width . "\" height=\"" . $height . "\" alt=\"" . $alt . "\" />\n";
 		return;
