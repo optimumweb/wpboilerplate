473557adc18792eb31ebb46e23f5e323ff47e6cf
Jonathan Roy <jroy66@gmail.com>
2011-08-24 13:08:09 -0400

    Implement a custom post thumbnail function 'wpbp_post_thumbnail' compatible with timthumb

--- inc/wpbp-extend.php
+++ inc/wpbp-extend.php
@@ -15,4 +15,17 @@ if ( !function_exists('single_author_title') ) {
 
 }
 
+if ( !function_exists('wpbp_post_thumbnail') ) {
+
+	function wpbp_post_thumbnail($post_ID, $width = 150, $height = 150, $quality = 0.9)
+	{
+		$url = get_post_meta( $post_ID, 'featured_image_url', true );
+		$alt = get_title( $post_ID );
+		$src = get_bloginfo('stylesheet_directory') . '/img/resize.php?w=' . $width . '&h=' . $height . '&q=' . $quality . '&src=' . $url;
+		echo "<img src=\"" . $src . "\" width=\"" . $width . "\" height=\"" . $height . "\" alt=\"" . $alt . "\" />\n";
+		return;
+	}
+
+}
+
 ?>
