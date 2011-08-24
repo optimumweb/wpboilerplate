c5ebcb3bffec1adfd1c30580fd5ffce50adcc91f
Jonathan Roy <jroy66@gmail.com>
2011-08-24 13:15:50 -0400

    Fixing custom post thumbnail resizing script path for the template and not the stylesheet

--- inc/wpbp-extend.php
+++ inc/wpbp-extend.php
@@ -31,7 +31,7 @@ if ( !function_exists('wpbp_post_thumbnail') ) {
 	{
 		$url = get_post_meta( $post_ID, 'featured_image_url', true );
 		$alt = get_the_title( $post_ID );
-		$src = get_bloginfo('stylesheet_directory') . '/img/resize.php?w=' . $width . '&h=' . $height . '&q=' . $quality . '&src=' . $url;
+		$src = get_bloginfo('template_directory') . '/img/resize.php?w=' . $width . '&h=' . $height . '&q=' . $quality . '&src=' . $url;
 		echo "<img src=\"" . $src . "\" width=\"" . $width . "\" height=\"" . $height . "\" alt=\"" . $alt . "\" />\n";
 		return;
 	}
