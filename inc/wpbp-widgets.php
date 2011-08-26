586e1c4b2212517349fd38cc0ec956408d247f05
Jonathan Roy <jroy66@gmail.com>
2011-08-26 18:00:30 -0400

    HOT COMMIT! Implementing a form builder as well as a widget to display most popular posts

--- inc/wpbp-widgets.php
+++ inc/wpbp-widgets.php
@@ -1,5 +1,41 @@
 <?php
 
+function wpbp_prepare_fields(&$fields, $prefix = 'wpbp-')
+{
+	foreach ( $fields as $key => $field ) {
+		$fields[$key]['id'] = isset($fields[$key]['id']) ? $fields[$key]['id'] : $prefix . $key;
+		$fields[$key]['name'] = isset($fields[$key]['name']) ? $fields[$key]['name'] : $prefix . $key;
+	}
+}
+
+function wpbp_build_form($fields, $current = null)
+{
+	wpbp_prepare_fields($fields);
+
+	foreach ( $fields as $key => $field ) {
+
+		extract($field);
+
+		echo "<p><label for=\"" . $id . "\">" . __($title, 'wpbp') . "</label> ";
+
+		if ( $type == 'text' ) {
+			$value = ( isset($current[$key]) ? $current[$key] : ( isset($defval) ? $defval : "" ) );
+			echo "<input type=\"text\" name=\"" . $name . "\" value=\"" . $value . "\" class=\"widefat\" id=\"" . $id . "\" />";
+		}
+
+		elseif ( $type == 'dropdown' ) {
+			echo "<select name=\"" . $name . "\" id=\"" . $id . "\">";
+			foreach ( $options as $optkey => $optval ) {
+				$selected = ( $current[$key] == $optkey ) ? " selected=\"selected\"" : "";
+				echo "<option value=\"" . $optkey . "\"" . $selected . ">" . $optval . "</option>";
+			}
+			echo "</select>";
+		}
+
+		echo "</p>" . PHP_EOL;
+	}
+}
+
 class wpbp_vcard extends WP_Widget {
 
 	function wpbp_vcard() {
@@ -66,7 +102,7 @@ class wpbp_vcard extends WP_Widget {
 		<p>
 			<label for="<?php echo $this->get_field_id('email'); ?>"><?php _e('Email:', 'wpbp'); ?></label>
 			<input type="text" name="<?php echo $this->get_field_name('email'); ?>" value="<?php echo esc_attr($instance['email']); ?>" class="widefat" id="<?php echo $this->get_field_id('email'); ?>" />
-		</p>                                   
+		</p>
 	<?php
 	}
 } 
@@ -118,5 +154,59 @@ class wpbp_cat_nav extends WP_Widget {
 }
 
 register_widget('wpbp_cat_nav');
+
+
+class wpbp_most_popular extends WP_Widget {
+
+	function wpbp_most_popular() {
+		$widget_ops = array('description' => 'Displays the most popular posts based on number of views in the last \'x\' days.');
+		parent::WP_Widget(false, __('WPBP: Most Popular', 'wpbp'), $widget_ops);
+	}
+
+	function widget($args, $instance) {
+		extract($args);
+		extract($instance);
+
+		echo $before_widget;
+		if ($title) {
+			echo $before_title . $title . $after_title;
+		}
+
+		$category = get_query_var('cat');
+
+		echo $after_widget;
+
+	}
+
+	function update($new_instance, $old_instance) {
+		return $new_instance;
+	}
+
+	function form($instance) {
+		$fields = array(
+			'title' => array(
+				'title' => 'Title:',
+				'type' => 'text',
+				'defval' => '',
+				'required' => false
+			),
+			'time_range' => array(
+				'title' => 'Time range:',
+				'type' => 'dropdown',
+				'defaval' => 'today',
+				'required' => true,
+				'options' => array(
+					'today' => 'Today',
+					'3-days' => 'Last 3 days',
+					'7-days' => 'Last week',
+					'all-time' => 'All-time'
+				)
+			)
+		);
+		wpbp_build_form($fields, $instance);
+	}
+}
+
+register_widget('wpbp_most_popular');
 
 ?>
