e7270c1ec0a3d79656302ce5fd76273cf3483620
Jonathan Roy <jroy66@gmail.com>
2011-08-26 19:36:47 -0400

    Stop troubleshooting wp_query

--- header.php
+++ header.php
@@ -24,8 +24,6 @@
 
 <body <?php $page_slug = $post->post_name; body_class($page_slug); ?>>
 
-	<?php var_dump($wp_query); ?>
-
 	<?php wpbp_wrap_before(); ?>
 	<div id="wrap" role="document">
 		<?php wpbp_header_before(); ?>
