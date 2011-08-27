9b59f23cb998695efb90ee476019a4ed76b11f8f
Jonathan Roy <jroy66@gmail.com>
2011-08-27 03:29:31 -0400

    Troubleshooting view counting plugin

--- inc/wpbp-actions.php
+++ inc/wpbp-actions.php
@@ -159,8 +159,8 @@ function wpbp_count_view()
 {
 	global $wp_query;
 	if ( is_single() && isset($wp_query->post->ID) ) {
-		wpbp_set_the_views( $wp_query->post->ID, date('Y-m-d') );
-		echo "Counted view!";
+		$view_counted = wpbp_set_the_views( $wp_query->post->ID, date('Y-m-d') );
+		if ( $view_counted ) echo "Counted view!";
 	}
 }
 
