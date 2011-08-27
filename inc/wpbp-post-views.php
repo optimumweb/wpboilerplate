0fddf5064959cfa2e46c88728e1d842788aef678
Jonathan Roy <jroy66@gmail.com>
2011-08-27 03:39:34 -0400

    Troubleshooting view counting plugin

--- inc/wpbp-post-views.php
+++ inc/wpbp-post-views.php
@@ -24,7 +24,11 @@ function wpbp_get_the_views($post_ID, $start = null, $end = null)
 function wpbp_set_the_views($post_ID, $date = null)
 {
 	$date = ( !is_null($date) ) ? $date : date('Y-m-d');
-	list($year, $month, $day) = array_map( 'intval', explode('-', $date) );
+	$date_arr = array_map( 'intval', explode('-', $date) );
+
+	var_dump($date_arr);
+
+	list($year, $month, $day) = array(0,0,0);
 
 	if ( !checkdate( $month, $day, $year ) ) return false;
 
