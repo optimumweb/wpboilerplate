354b62c7a11f77941c9f8068e72224ed92b07762
Jonathan Roy <jroy66@gmail.com>
2011-08-30 16:40:50 -0400

    Fixing most_popular posts widget to order by date and post_views

--- inc/wpbp-widgets.php
+++ inc/wpbp-widgets.php
@@ -362,9 +362,9 @@ class wpbp_most_popular extends WP_Widget {
 		) );
 
 		echo "<ul class=\"wpbp-most-popular\">";
-		foreach( $posts as $post ) : setup_postdata($post);
-			?><li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li><?php
-		endforeach; wp_reset_postdata();
+		foreach( $posts as $post ) {
+			echo "<li><a href=\"" . get_permalink( $post->ID ) . "\">" . $post->post_title . "</a></li>";
+		}
 		echo "</ul>";
 
 		$chrono = microtime(true) - $chrono;
