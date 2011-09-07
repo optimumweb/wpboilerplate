3398dba9d81ddd6222a499bc143db74e516948f7
Jonathan Roy <jroy66@gmail.com>
2011-09-06 23:25:29 -0400

    Improved wpbp_custom_breadcrumb

--- inc/wpbp-breadcrumb.php
+++ inc/wpbp-breadcrumb.php
@@ -20,9 +20,7 @@ function wpbp_custom_breadcrumb($sep = ' &rarr; ', $before = '', $after = '', $b
 				$post_type = get_post_type_object( get_post_type() );
 				echo $before_item . $post_type->labels->name . $after_item;
 
-				echo $before_item;
-				the_title();
-				echo $after_item;
+				echo $before_item . get_the_title() . $after_item;
 			}
 
 			else {
@@ -45,9 +43,7 @@ function wpbp_custom_breadcrumb($sep = ' &rarr; ', $before = '', $after = '', $b
 			}
 			echo $page_parents;
 
-			echo $before_item;
-			the_title();
-			echo $after_item;
+			echo $before_item . get_the_title() . $after_item;
 		}
 
 		elseif ( is_category() ) {
