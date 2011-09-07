22f83f8857938558d4a23bfe0a97251fe12cb7bf
Jonathan Roy <jroy66@gmail.com>
2011-09-06 23:24:55 -0400

    Improved wpbp_custom_breadcrumb

--- inc/wpbp-breadcrumb.php
+++ inc/wpbp-breadcrumb.php
@@ -19,6 +19,10 @@ function wpbp_custom_breadcrumb($sep = ' &rarr; ', $before = '', $after = '', $b
 			if ( in_array( get_post_type(), get_post_types( array( 'public' => true, '_builtin' => false ), 'names', 'and' ) ) ) {
 				$post_type = get_post_type_object( get_post_type() );
 				echo $before_item . $post_type->labels->name . $after_item;
+
+				echo $before_item;
+				the_title();
+				echo $after_item;
 			}
 
 			else {
