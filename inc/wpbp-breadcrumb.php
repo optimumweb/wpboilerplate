d2dece5c5d632f8ee897e5f0907a5379451b0e1e
Jonathan Roy <jroy66@gmail.com>
2011-09-06 23:03:01 -0400

    Improved wpbp_custom_breadcrumb

--- inc/wpbp-breadcrumb.php
+++ inc/wpbp-breadcrumb.php
@@ -23,11 +23,20 @@ function wpbp_custom_breadcrumb($sep = ' &rarr; ', $before = '', $after = '', $b
 			}
 
 			elseif ( is_single() ) {
-				$categories = get_the_category();
-				$category = $categories[0];
-				echo $before_item . "<a href=\"" . get_category_link( $category->cat_ID ) . "\">" . $category->cat_name . "</a>" . $after_item;
-				echo $sep;
-				echo $before_item . get_the_title() . $after_item;
+
+				if ( in_array( get_post_type(), get_post_types( array( 'public' => true, '_builtin' => false ), 'names', 'and' ) ) ) {
+					$post_type = get_post_type_object( get_post_type() );
+					echo $before_item . $post_type->labels->name . $after_item . 'xxxxxx';
+				}
+
+				else {
+					$categories = get_the_category();
+					$category = $categories[0];
+					echo $before_item . "<a href=\"" . get_category_link( $category->cat_ID ) . "\">" . $category->cat_name . "</a>" . $after_item;
+					echo $sep;
+					echo $before_item . get_the_title() . $after_item;
+				}
+
 			}
 		}
 
@@ -47,11 +56,6 @@ function wpbp_custom_breadcrumb($sep = ' &rarr; ', $before = '', $after = '', $b
 			echo $before_item;
 			the_search_query();
 			echo $after_item;
-		}
-
-		elseif ( in_array( get_post_type(), get_post_types( array( 'public' => true, '_builtin' => false ), 'names', 'and' ) ) ) {
-			$post_type = get_post_type_object( get_post_type() );
-			echo $before_item . $post_type->labels->name . $after_item . 'xxxxxx';
 		}
 
 		elseif ( is_page() ) {
