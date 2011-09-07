4585abab91ce48ce6ee4ee18b676b45171921ce0
Jonathan Roy <jroy66@gmail.com>
2011-09-06 22:38:02 -0400

    Improved wpbp_custom_breadcrumb

--- inc/wpbp-breadcrumb.php
+++ inc/wpbp-breadcrumb.php
@@ -1,10 +1,10 @@
 <?php
 
-function wpbp_custom_breadcrumb($before = '', $after = '') {
+function wpbp_custom_breadcrumb($before = '', $after = '', $sep = ' &rarr; ', $before_item = '', $after_item = '') {
 
 	echo $before;
 
-	echo "<a href=\"" . get_option('home') . "\">" . get_bloginfo('name') . "</a>";
+	echo $before_item . "<a href=\"" . get_option('home') . "\">" . get_bloginfo('name') . "</a>" . $after_item;
 	
 	if ( !is_home() && !is_front_page() ) {
 
@@ -12,7 +12,7 @@ function wpbp_custom_breadcrumb($before = '', $after = '') {
 
 		$post_ID = $wp_query->post->ID;
 
-		echo " &rarr; ";
+		echo $sep;
 
 		if ( is_category() || is_single() ) {
 
@@ -23,21 +23,28 @@ function wpbp_custom_breadcrumb($before = '', $after = '') {
 			if ( is_single() ) {
 				$categories = get_the_category();
 				$category = $categories[0];
-				echo "<a href=\"" . get_category_link( $category->cat_ID ) . "\">" . $category->cat_name . "</a>";
-				echo " &rarr; " . get_the_title();
+				echo $before_item . "<a href=\"" . get_category_link( $category->cat_ID ) . "\">" . $category->cat_name . "</a>" . $after_item;
+				echo $sep;
+				echo $before_item . get_the_title() . $after_item;
 			}
 		}
 
 		elseif ( is_tag() ) {
+			echo $before_item;
 			single_tag_title();
+			echo $after_item;
 		}
 
 		elseif ( is_author() ) {
+			echo $before_item;
 			single_author_title();
+			echo $after_item;
 		}
 
 		elseif ( is_search() ) {
+			echo $before_item;
 			the_search_query();
+			echo $after_item;
 		}
 
 		elseif ( is_page() ) {
@@ -45,13 +52,15 @@ function wpbp_custom_breadcrumb($before = '', $after = '') {
 			$page_parents = "";
 			$page_parent_ID = $wp_query->post->post_parent;
 			while ( $page_parent_ID ) {
-				$page_parents = "<a href=\"" . get_permalink( $page_parent_ID ) . "\">" . get_the_title( $page_parent_ID ) . "</a> &rarr; " . $page_parents;
+				$page_parents = $before_item . "<a href=\"" . get_permalink( $page_parent_ID ) . "\">" . get_the_title( $page_parent_ID ) . "</a>" . $after_item . $sep . $page_parents;
 				$page_parent = get_page( $page_parent_ID );
 				$page_parent_ID = $page_parent->post_parent;
 			}
 			echo $page_parents;
-			
+
+			echo $before_item;
 			the_title();
+			echo $after_item;
 		}
 
 	}
