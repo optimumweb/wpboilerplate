759da4de00f30f9a15ada0de56a004feb434715a
Jonathan Roy <jroy@optimumweb.ca>
2011-10-18 16:31:52 +0000

    Use jSlider.js (the non-minified version) for dev purposes

--- inc/wpbp-actions.php
+++ inc/wpbp-actions.php
@@ -1,78 +1,4 @@
-<?php
-
-add_action('wpbp_head', 'wpbp_og_tags');
-add_action('wpbp_head', 'wpbp_google_analytics');
-add_action('wpbp_head', 'wpbp_custom_css');
-add_action('wpbp_stylesheets', 'wpbp_get_stylesheets');
-add_action('wpbp_scripts', 'wpbp_get_scripts');
-add_action('wpbp_breadcrumb', 'wpbp_get_breadcrumb');
-add_action('wpbp_footer', 'wpbp_count_view');
-
-function wpbp_google_analytics()
-{
-	global $wpbp_options;
-	$wpbp_google_analytics_id = $wpbp_options['google_analytics_id'];
-	$wpbp_get_google_analytics_id = esc_attr($wpbp_options['google_analytics_id']);
-	if ($wpbp_google_analytics_id !== '') {
-?>
-<script type="text/javascript">
-	var _gaq = _gaq || [];
-	_gaq.push(['_setAccount', '<?php echo $wpbp_google_analytics_id; ?>']);
-	_gaq.push(['_trackPageview']);
-	_gaq.push(['_trackPageLoadTime']);
-	(function() {
-		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-	})();
-</script>
-<?php
-	}
-	return;
-}
-
-function wpbp_og_tags()
-{
-	global $wp_query;
-
-	if ( is_single() ) {
-		$current_post = get_post( $wp_query->post->ID );
-		$current_post_image = wpbp_get_post_image( $wp_query->post->ID );
-	}
-
-	$og = array(
-		'title'			=> is_single() ? $current_post->post_title : wp_title('', false),
-		'url'			=> 'http://' . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"],
-		'image'			=> is_single() ? $current_post_image['url'] : false,
-		'site_name'		=> get_bloginfo('name'),
-		'description'	=> is_single() ? $current_post->post_excerpt : false,
-	);
-
-	foreach ( $og as $key => $val ) {
-		echo "<meta property=\"og:" . $key . "\" content=\"" . $val . "\" />\n";
-	}
-	
-	return;
-}
-
-function wpbp_get_scripts()
-{
-
-	global $wpbp_options;
-
-	$scripts = "";
-
-	if ( $wpbp_options['js_plugins']['modernizr'] ) {
-		$scripts .= script_tag('http://firecdn.net/libs/modernizr/2.0.6/modernizr.min.js');
-	}
-
-	if ( $wpbp_options['js_plugins']['lesscss'] ) {
-		$scripts .= script_tag('http://firecdn.net/libs/less/1.1.3/less.min.js');
-	}
-
-	if ( $wpbp_options['js_plugins']['jquery'] ) {
-
-		$scripts .= script_tag('http://firecdn.net/libs/jquery/1.6.3/jquery.min.js');
+.min.js');
 
 		if ( $wpbp_options['js_plugins']['formalize'] ) {
 			$scripts .= script_tag('http://firecdn.net/libs/formalize/js/jquery.formalize.js');
@@ -83,7 +9,7 @@ function wpbp_get_scripts()
 		}
 
 		if ( $wpbp_options['js_plugins']['jSlider'] ) {
-			$scripts .= script_tag('http://firecdn.net/libs/jSlider/jquery.jSlider.min.js');
+			$scripts .= script_tag('http://firecdn.net/libs/jSlider/jquery.jSlider.js');
 		}
 	}
 
