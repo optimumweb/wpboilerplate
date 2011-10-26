a594d8aa1b0bc4396457c940925fba20c74317eb
Jonathan Roy <jroy@optimumweb.ca>
2011-10-26 17:37:52 +0000

    Blackbox function to set post_ID

--- inc/wpbp-extend.php
+++ inc/wpbp-extend.php
@@ -1,5 +1,17 @@
 <?php
 
+if ( !function_exists('set_post_ID') ) {
+    
+    function set_post_ID(&$post_ID)
+	{
+		if ( !isset($post_ID) || !is_int($post_ID) ) {
+            global $wp_query;
+            $post_ID = $wp_query->post->ID;
+        }
+	}
+    
+}
+
 if ( !function_exists('wpbp_get_author') ) {
 
 	function wpbp_get_author($field = null, $ID = null)
@@ -42,6 +54,94 @@ if ( !function_exists('wpbp_has_post_thumbnail') ) {
             if ( strlen( $url ) > 0 ) return true;
 		}
         return false;
+	}
+
+}
+
+if ( !function_exists('wpbp_get_full_url') ) {
+    
+    function wpbp_get_full_url($url)
+    {
+        if ( strpos($url, 'http') === false ) {
+    		$protocol = ( isset($_SERVER['HTTPS']) ) ? 'https' : 'http';
+			$url = $protocol . '://' . $_SERVER['SERVER_NAME'] . $url;
+		}
+        return $url;
+    }
+    
+}
+
+if ( !function_exists('wpbp_is_valid_image') ) {
+
+    function wpbp_is_valid_image($url, $valid_image_types = array( IMAGETYPE_GIF, IMAGETYPE_JPEG, IMAGETYPE_PNG ))
+	{
+        if ( !is_array($valid_image_types) ) return null;
+        
+        $url = wpbp_get_full_url($url);
+        
+        if ( function_exists('exif_imagetype') ) {
+            $image_type = exif_imagetype($url);
+        }
+        else {
+            $image_attr = @getimagesize($url);
+            if ( isset($image_attr) && is_array($image_attr) ) {
+                $image_type = $image_attr[2];
+            }
+        }
+        if ( isset($image_type) && in_array($image_type, $valid_image_types) ) {
+            return true;
+        }
+        return false;
+	}
+    
+}
+
+if ( !function_exists('wpbp_get_image_size') ) {
+
+	function wpbp_get_image_size($url)
+	{
+		$url = wpbp_get_full_url($url);
+        
+        if ( wpbp_is_valid_image($url) ) {
+
+    		$image_attr = @getimagesize($url);
+    
+    		if ( isset($image_attr) && is_array($image_attr) ) {
+    			list($width, $height, $type, $attr) = $image_attr;
+    			$ratio = round( $width / $height );
+    			return compact('url', 'width', 'height', 'ratio', 'type', 'attr');
+    		}
+        
+        }
+        
+		return false;
+	}
+
+}
+
+if ( !function_exists('wpbp_resize_image_url') ) {
+
+	function wpbp_resize_image_url($url, $width = 'auto', $height = 'auto', $q = '90')
+	{
+		$image_attr = wpbp_get_image_size($url);
+
+		if ( isset($image_attr) && is_array($image_attr) ) {
+
+			if ( $width == 'auto' && $height == 'auto' ) {
+				$width = $image_attr['width'];
+				$height = $image_attr['height'];
+			}
+			elseif ( $height == 'auto' ) {
+				$height = round( $width / $image_attr['ratio'] );
+			}
+			elseif ( $width == 'auto' ) {
+				$width = round( $height * $image_attr['ratio'] );
+			}
+
+			return get_bloginfo('template_directory') . '/img/resize.php?w=' . $width . '&h=' . $height . '&q=' . $q . '&src=' . $url;
+		}
+
+		return false;
 	}
 
 }
