a10640392f05df9d24edaec9c2d7e99c89050494
Jonathan Roy <jroy@optimumweb.ca>
2011-10-31 17:27:51 +0000

    Split header and insert head.php

---
+++ head.php
@@ -0,0 +1,26 @@
+<!doctype html>
+
+<!--[if lt IE 7]> <html class="no-js ie6 oldie" <?php language_attributes(); ?>> <![endif]-->
+<!--[if IE 7]>    <html class="no-js ie7 oldie" <?php language_attributes(); ?>> <![endif]-->
+<!--[if IE 8]>    <html class="no-js ie8 oldie" <?php language_attributes(); ?>> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" <?php language_attributes(); ?>> <!--<![endif]-->
+
+<head>
+    
+<meta charset="utf-8">
+
+<title><?php wp_title(''); ?></title>
+
+<?php wpbp_stylesheets(); ?>
+
+<link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> Feed" href="<?php echo home_url(); ?>/feed/">
+
+<?php wpbp_scripts(); ?>
+
+<?php wp_head(); ?>
+
+<?php wpbp_head(); ?>
+
+</head>
+
+<body <?php $page_slug = $post->post_name; body_class($page_slug); ?>>
--- header.php
+++ header.php
@@ -1,28 +1,6 @@
 <?php global $wpbp_options; ?>
-<!doctype html>
-<!--[if lt IE 7]> <html class="no-js ie6 oldie" <?php language_attributes(); ?>> <![endif]-->
-<!--[if IE 7]>    <html class="no-js ie7 oldie" <?php language_attributes(); ?>> <![endif]-->
-<!--[if IE 8]>    <html class="no-js ie8 oldie" <?php language_attributes(); ?>> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js" <?php language_attributes(); ?>> <!--<![endif]-->
-<head>
-	
-<meta charset="utf-8">
 
-<title><?php wp_title(''); ?></title>
-
-<?php wpbp_stylesheets(); ?>
-
-<link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> Feed" href="<?php echo home_url(); ?>/feed/">
-
-<?php wpbp_scripts(); ?>
-
-<?php wp_head(); ?>
-
-<?php wpbp_head(); ?>
-
-</head>
-
-<body <?php $page_slug = $post->post_name; body_class($page_slug); ?>>
+<?php get_template_part('head'); ?>
 
 	<?php wpbp_wrap_before(); ?>
 	<div id="wrap" role="document">
