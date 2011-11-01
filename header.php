9fbcbf103eec0755a66082cca81e72040444b44f
Jonathan Roy <jroy@optimumweb.ca>
2011-11-01 15:24:52 +0000

    Fix container architecture

--- header.php
+++ header.php
@@ -6,16 +6,18 @@
 		<?php wpbp_header_before(); ?>
 		<header id="header" role="banner">
 			<?php wpbp_header_inside(); ?>
-            <h1 class="site-title">
-                <a href="<?php echo home_url(); ?>/"><?php bloginfo('name'); ?></a>
-				<span><?php bloginfo('description'); ?></span>
-			</h1>
-			<nav id="main-nav" role="navigation">
-				<div class="container <?php echo $wpbp_options['container_class']; ?>">
-					<?php dynamic_sidebar("Nav"); ?>
-					<div class="clear"></div>
-				</div>
-			</nav>
+            <div class="container <?php echo $wpbp_options['container_class']; ?>">
+                <h1 class="site-title">
+                    <a href="<?php echo home_url(); ?>/"><?php bloginfo('name'); ?></a>
+				    <span><?php bloginfo('description'); ?></span>
+			    </h1>
+			    <nav id="main-nav" role="navigation">
+			    	<div class="container">
+				    	<?php dynamic_sidebar("Nav"); ?>
+				    	<div class="clear"></div>
+			    	</div>
+			    </nav>
+            </div>
 		</header>
 		<div class="clear"></div>
 		<?php wpbp_header_after(); ?>
