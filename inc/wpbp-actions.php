a7798a00b6d76204c45b7657ea038bd273f80593
Jonathan Roy <jroy@optimumweb.ca>
2011-11-11 21:08:05 +0000

    Major update on function names

--- inc/wpbp-actions.php
+++ inc/wpbp-actions.php
@@ -187,6 +187,7 @@ function wpbp_validate_featured_image_url($post_ID)
     if ( has_featured_image($post_ID) ) {
         $featured_image_url = get_featured_image($post_ID);
         if ( !wpbp_is_valid_image($featured_image_url) ) {
+            add_action('admin_notices', create_function('', "echo '<div class=\"error\"><p>" . __('Please make sure your featured image url is valid', 'wpbp') . "</p></div>';"));
             return update_post_meta($post_ID, 'featured_image_url', '', $featured_image_url);
         }
     }
