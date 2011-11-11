a4dc14e239d286c5d1c518336dcd35b3782620cd
Jonathan Roy <jroy@optimumweb.ca>
2011-11-11 01:05:20 +0000

    Huge feature implementation with wpbp_images table

--- inc/wpbp-extend.php
+++ inc/wpbp-extend.php
@@ -46,6 +46,44 @@ if ( !function_exists('single_author_title') ) {
 
 }
 
+    
+    function wpbp_table_exists($table_name)
+    {
+        global $wpdb;
+        
+        $sql = @$wpdb->query("SELECT * FROM " . $table_name . " LIMIT 1");
+        
+        return ( !$sql ) ? false : true;
+    }
+    
+    function wpbp_image_table_exists()
+    {
+        return wpbp_table_exists('wpbp_images');
+    }
+    
+    function wpbp_create_image_table()
+    {
+        global $wpdb;
+        
+        if ( wpbp_image_table_exists() ) return false;
+        
+        $wpbp_image_table_name = 'wpbp_images';
+        
+        $sql = $wpdb->query("
+            CREATE TABLE IF NOT EXISTS " . $prefix . $wpbp_image_table_name . " (
+                ID       INT NOT NULL AUTO_INCREMENT, PRIMARY KEY (ID), INDEX (ID),
+                url      VARCHAR(255) NOT NULL, UNIQUE (url),
+                width    SMALLINT,
+                height   SMALLINT,
+                ratio    FLOAT,
+                type     TINYINT,
+                status   TINYINT NOT NULL
+            ) ENGINE = MyISAM;
+        ");
+        
+        return $sql;
+    }
+
 if ( !function_exists('wpbp_has_post_thumbnail') ) {
 
 	function wpbp_has_post_thumbnail($post_ID)
