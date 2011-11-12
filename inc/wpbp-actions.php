311afc3e81f02d5d952ba32d9f5e02753d2c0a78
Jonathan Roy <jroy@optimumweb.ca>
2011-11-12 22:23:06 +0000

    Validate featured image url

--- inc/wpbp-actions.php
+++ inc/wpbp-actions.php
@@ -214,8 +214,7 @@ function wpbp_validate_featured_image_url($post_ID)
     if ( has_featured_image($post_ID) ) {
         $featured_image_url = get_featured_image($post_ID);
         if ( !wpbp_is_valid_image($featured_image_url) ) {
-            add_action('admin_notices', create_function('', "echo '<div class=\"error\"><p>" . __('Please make sure your featured image url is valid', 'wpbp') . "</p></div>';"));
-            //add_delayed_admin_notice(__('Please make sure your featured image url is valid', 'wpbp'), 'error', $_GET['post']);
+            add_delayed_admin_notice(__('Please make sure your featured image url is valid', 'wpbp'), 'error', $_GET['post']);
             return update_post_meta($post_ID, 'featured_image_url', '', $featured_image_url);
         }
     }
