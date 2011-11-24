a234de99da3e12f402b93d192003db6ddf92d54f
Jonathan Roy <jroy@optimumweb.ca>
2011-11-24 07:13:48 +0000

    Drop chrome frame usage

--- footer.php
+++ footer.php
@@ -21,10 +21,5 @@
 <?php wp_footer(); ?>
 <?php wpbp_footer(); ?>
 
-	<!--[if lt IE 7]>
-		<script src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js"></script>
-		<script>window.attachEvent('onload',function(){CFInstall.check({mode:'overlay'})})</script>
-	<![endif]-->
-
 </body>
 </html>
