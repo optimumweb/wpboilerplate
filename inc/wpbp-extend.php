299f14be2f114752a4fdfcd4386016bb79b01299
Jonathan Roy <jroy@optimumweb.ca>
2011-12-30 19:41:57 +0000

    New minimal nav menu walker

--- inc/wpbp-extend.php
+++ inc/wpbp-extend.php
@@ -236,4 +236,33 @@ class Description_Walker extends Walker_Nav_Menu
     }
 }
 
+class Minimal_Walker extends Walker_Nav_Menu
+{
+    function start_el(&$output, $item, $depth, $args)
+    {
+        global $wp_query;
+        $indent = ( $depth ) ? str_repeat( "\t", $depth ) : '';
+        
+        $class_names = $value = '';
+        
+        $classes = empty( $item->classes ) ? array() : (array) $item->classes;
+        
+        $class_names = join( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item ) );
+        $class_names = ' class="'. esc_attr( $class_names ) . '"';
+        
+        $attributes  = ! empty( $item->attr_title ) ? ' title="'  . esc_attr( $item->attr_title ) .'"' : '';
+        $attributes .= ! empty( $item->target )     ? ' target="' . esc_attr( $item->target     ) .'"' : '';
+        $attributes .= ! empty( $item->xfn )        ? ' rel="'    . esc_attr( $item->xfn        ) .'"' : '';
+        $attributes .= ! empty( $item->url )        ? ' href="'   . esc_attr( $item->url        ) .'"' : '';
+        
+        $item_output .= '<a'. $class_names .'" '. $attributes .'>';
+        $item_output .= $args->link_before;
+        $item_output .= apply_filters( 'the_title', $item->title, $item->ID );
+        $item_output .= $args->link_after;
+        $item_output .= '</a>';
+        
+        $output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
+    }
+}
+
 ?>
