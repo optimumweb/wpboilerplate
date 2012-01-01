6feec4999ceccbaa9fad475ba5d9a79b2b472deb
Jonathan Roy <jroy@optimumweb.ca>
2012-01-01 13:59:16 -0500

    Deleted mail file and fixed og tags

--- inc/wpbp-actions.php
+++ inc/wpbp-actions.php
@@ -41,11 +41,11 @@ function wpbp_og_tags()
     }
 
     $og = array(
-        'title'       => htmlentities(trim(wp_title('', false))),
+        'title'       => trim(wp_title('', false)),
         'url'         => wpbp_get_current_url(),
         'image'       => is_single() ? $current_post_image['url'] : null,
-        'site_name'   => htmlentities(get_bloginfo('name')),
-        'description' => is_single() ? htmlentities(strip_tags(wpbp_get_the_excerpt($wp_query->post->ID))) : null
+        'site_name'   => get_bloginfo('name'),
+        'description' => is_single() ? strip_tags(wpbp_get_the_excerpt($wp_query->post->ID)) : null
     );
 
     foreach ( $og as $key => $val ) {
