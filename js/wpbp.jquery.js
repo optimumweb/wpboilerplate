ec447d9c83d5ca4f1c97fae46e70ab0c708af419
Jonathan Roy <jroy@optimumweb.ca>
2012-01-03 12:32:15 -0500

    New valign plugin

--- js/wpbp.jquery.js
+++ js/wpbp.jquery.js
@@ -1,8 +1,28 @@
 $(document).ready(function() {
 	
 	/*
-	* IR plugin
-	* Replaces elements with 'ir' class with an image (specified by 'data-ir').
-	*/
-	$('.ir').each(function() { $(this).css('background-image', 'url(' + $(this).data('ir') + ')'); });
+	 * IR plugin
+	 * Replaces elements with the 'ir' class with an image specified by 'data-ir'.
+	 */
+	$('.ir').each(function() {
+		$(this).css('background-image', 'url(' + $(this).data('ir') + ')');
+	});
+	
+});
+
+$(window).load(function() {
+
+	/*
+	 * VALIGN plugin
+	 * Aligns vertically elements with the 'valign' class with reference either the parent element
+	 * or another element specified by 'data-ref'.
+	 */
+	$('.valign').each(function() {
+		$this = $(this);
+		$ref = ( typeof $this.data('ref') != 'undefined' ) ? $this.data('ref') : $this.parent();
+		thisHeight = $this.height(), refHeight = $ref.height();
+		offset = Math.round( ( refHeight - thisHeight ) / 2 );
+		$this.css('margin-top', offset + 'px').css('margin-bottom', offset + 'px');
+	});
+
 });
