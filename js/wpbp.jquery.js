c18617992b0e734924482dbe849ad792bb861cde
Jonathan Roy <jroy@optimumweb.ca>
2012-01-04 12:49:21 -0500

    Added ajax form js plugin

--- js/wpbp.jquery.js
+++ js/wpbp.jquery.js
@@ -12,143 +12,156 @@ $(window).load(function() {
 
 });
 
+/*
+ * IR plugin
+ *
+ * Replaces elements with the 'ir' class with an image specified by 'data-ir'.
+ * @author Jonathan Roy <jroy@optimumweb.ca>
+ * @version 1.0
+ * @package wpboilerplate
+ */
+ 
 (function($) {
-
-	/*
-	 * IR plugin
-	 *
-	 * Replaces elements with the 'ir' class with an image specified by 'data-ir'.
-	 * @author Jonathan Roy <jroy@optimumweb.ca>
-	 * @version 1.0
-	 * @package wpboilerplate
-	 */
-
-	$.fn.IR = function() {
-		var $this = $(this);
-		var ir = $this.data('ir');
-		if ( typeof if != 'undefined' ) {
-			$this.css('background-image', 'url(' + ir + ')');
+	$.fn.extend({
+		IR: function() {
+			var $this = $(this);
+			var ir = $this.data('ir');
+			if ( typeof if != 'undefined' ) {
+				$this.css('background-image', 'url(' + ir + ')');
+			}
 		}
-	}
+	});
+})(jQuery);
 
-	/*
-	 * VALIGN plugin
-	 *
-	 * Aligns vertically elements with the 'valign' class with reference either the parent element
-	 * or another element specified by 'data-ref'.
-	 * @author Jonathan Roy <jroy@optimumweb.ca>
-	 * @version 1.0
-	 * @package wpboilerplate
-	 */
-	 
-	$.fn.vAlign = function() {
-		var $this = $(this);
-		var $ref = ( typeof $this.data('ref') != 'undefined' ) ? $($this.data('ref')) : $this.parent();
-		var thisHeight = $this.outerHeight(true), refHeight = $ref.outerHeight(true);
-		var offset = Math.round( ( refHeight - thisHeight ) / 2 );
-		$this.css('margin-top', offset + 'px').css('margin-bottom', offset + 'px');
-	}
 
-	/*
-	 * AJAX FORM plugin
-	 *
-	 * Validate and send a form asynchronously
-	 * @author Jonathan Roy <jroy@optimumweb.ca>
-	 * @version 2.0
-	 * @package wpboilerplate
-	 */
+/*
+ * VALIGN plugin
+ *
+ * Aligns vertically elements with the 'valign' class with reference either the parent element
+ * or another element specified by 'data-ref'.
+ * @author Jonathan Roy <jroy@optimumweb.ca>
+ * @version 1.0
+ * @package wpboilerplate
+ */
 
-	$.fn.ajaxForm = function() {
+(function($) {
+	$.fn.extend({
+		vAlign: function() {
+			var $this = $(this);
+			var $ref = ( typeof $this.data('ref') != 'undefined' ) ? $($this.data('ref')) : $this.parent();
+			var thisHeight = $this.outerHeight(true), refHeight = $ref.outerHeight(true);
+			var offset = Math.round( ( refHeight - thisHeight ) / 2 );
+			$this.css('margin-top', offset + 'px').css('margin-bottom', offset + 'px');
+		}
+	});
+})(jQuery);
 
-		// define form elements
-		var $form = $(this);
-		var $formSuccess = $form.find(".success");
-		var $formError = $form.find(".error");
-		var $formLoading = $form.find(".loading");
 
-		// define form properties
-		var formId = $form.attr("id");
-		var formAction = $form.attr("action");
+/*
+ * AJAX FORM plugin
+ *
+ * Validate and send a form asynchronously
+ * @author Jonathan Roy <jroy@optimumweb.ca>
+ * @version 2.0
+ * @package wpboilerplate
+ */
 
-		// hide response messages and loading
-		$formSuccess.hide();
-		$formError.hide();
-		$formLoading.hide();
+(function($) {
+	$.fn.extend({
+	
+		ajaxForm: function() {
 
-		$form.submit(function(e) {
-
-			// show that we are working in the background
-			$formLoading.show();
-
-			// assume no errors in submission
-			var inputError = false;
-			var inputErrorMsg = "";
-
-			// validate all required fields
-			$form.find(".required").each(function() {
-				var $input = $(this);
-				if ( $input.val() == "" ) {
-					inputError = true;
-					$input.removeClass("valid");
-					$input.addClass("invalid");
-				}
-				else {
-					$input.removeClass("invalid");
-					$input.addClass("valid");
-				}
-			});
-
-			// validate emails
-			$form.find(".valid.email").each(function() {
-				var $input = $(this);
-				var emailRegex = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
-				if ( !emailRegex.test( $input.val() ) ) {
-					inputError = true;
-					$input.removeClass("valid");
-					$input.addClass("invalid");
-				}
-				else {
-					$input.removeClass("invalid");
-					$input.addClass("valid");
-				}
-			});
-
-			if ( !inputError ) {
-				$.ajax({
-					type:	'POST',
-					url:	formAction,
-					data:	$form.serialize(),
-					success: function(response) {
-						if ( response == "success" ) {
-							$formSuccess.fadeIn();
-							$formError.hide();
-							$form.children().not($formSuccess).hide();
-						}
-						else {
-							$formSuccess.hide();
-							$formError.fadeIn();
-						}
-						// trigger google analytics
-						_gaq.push(['_trackPageview', '/form-sent/' + formId]);
+			// define form elements
+			var $form = $(this);
+			var $formSuccess = $form.find(".success");
+			var $formError = $form.find(".error");
+			var $formLoading = $form.find(".loading");
+	
+			// define form properties
+			var formId = $form.attr("id");
+			var formAction = $form.attr("action");
+	
+			// hide response messages and loading
+			$formSuccess.hide();
+			$formError.hide();
+			$formLoading.hide();
+	
+			$form.submit(function(e) {
+	
+				// show that we are working in the background
+				$formLoading.show();
+	
+				// assume no errors in submission
+				var inputError = false;
+				var inputErrorMsg = "";
+	
+				// validate all required fields
+				$form.find(".required").each(function() {
+					var $input = $(this);
+					if ( $input.val() == "" ) {
+						inputError = true;
+						$input.removeClass("valid");
+						$input.addClass("invalid");
+					}
+					else {
+						$input.removeClass("invalid");
+						$input.addClass("valid");
 					}
 				});
-			}
-			else {
-				$formSuccess.hide();
-				$formError.fadeIn();
-			}
+	
+				// validate emails
+				$form.find(".valid.email").each(function() {
+					var $input = $(this);
+					var emailRegex = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+					if ( !emailRegex.test( $input.val() ) ) {
+						inputError = true;
+						$input.removeClass("valid");
+						$input.addClass("invalid");
+					}
+					else {
+						$input.removeClass("invalid");
+						$input.addClass("valid");
+					}
+				});
+	
+				if ( !inputError ) {
+					$.ajax({
+						type:	'POST',
+						url:	formAction,
+						data:	$form.serialize(),
+						success: function(response) {
+							if ( response == "success" ) {
+								$formSuccess.fadeIn();
+								$formError.hide();
+								$form.children().not($formSuccess).hide();
+							}
+							else {
+								$formSuccess.hide();
+								$formError.fadeIn();
+							}
+							// trigger google analytics
+							_gaq.push(['_trackPageview', '/form-sent/' + formId]);
+						}
+					});
+				}
+				else {
+					$formSuccess.hide();
+					$formError.fadeIn();
+				}
+	
+				// hide the loading
+				$formLoading.hide();
+	
+				// prevent default page load
+				e.preventDefault();
+	
+			});
+			
+			// show that the form is ajax-enabled
+			$form.addClass('ajax-enabled');
 
-			// hide the loading
-			$formLoading.hide();
-
-			// prevent default page load
-			e.preventDefault();
-
-		});
+		}
 		
-		// show that the form is ajax-enabled
-		$form.addClass('ajax-enabled');
-
-	}
+	});
 
 })(jQuery);
