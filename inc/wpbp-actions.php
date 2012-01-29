ba726b111062adc84d97adaecf149cf29d28b3c9
Jonathan Roy <jroy@optimumweb.ca>
2012-01-29 00:24:18 -0500

    Add styles correctly

--- inc/wpbp-actions.php
+++ inc/wpbp-actions.php
@@ -147,7 +147,21 @@ function wpbp_add_style($handle, $src = false, $deps = array(), $ver = false, $m
 	wp_enqueue_style($handle, $src, $deps, $ver, $media);
 }
 
-apply_filters( 'style_loader_tag', "<link rel='stylesheet' href='$href' type='text/css' media='$media' data-test='1' />\n", $handle );
+function enqueue_less_styles($tag, $handle) {
+    global $wp_styles;
+    $match_pattern = '/\.less$/U';
+    if ( preg_match( $match_pattern, $wp_styles->registered[$handle]->src ) ) {
+        $handle = $wp_styles->registered[$handle]->handle;
+        $media = $wp_styles->registered[$handle]->args;
+        $href = $wp_styles->registered[$handle]->src . '?ver=' . $wp_styles->registered[$handle]->ver;
+        $rel = isset($wp_styles->registered[$handle]->extra['alt']) && $wp_styles->registered[$handle]->extra['alt'] ? 'alternate stylesheet' : 'stylesheet';
+        $title = isset($wp_styles->registered[$handle]->extra['title']) ? "title='" . esc_attr( $wp_styles->registered[$handle]->extra['title'] ) . "'" : '';
+
+        $tag = "<link rel='stylesheet' id='$handle' $title href='$href' type='text/less' media='$media' />";
+    }
+    return $tag;
+}
+add_filter('style_loader_tag', 'enqueue_less_styles', 5, 2);
 
 function stylesheet_link_tag($args)
 {
