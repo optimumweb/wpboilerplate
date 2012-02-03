650127be468e8402632b2ccfe08495f8ae89f8b7
Jonathan Roy <jroy@optimumweb.ca>
2012-02-03 18:21:46 -0500

    Remove double spaces from link

--- inc/wpbp-actions.php
+++ inc/wpbp-actions.php
@@ -147,18 +147,18 @@ function wpbp_enqueue_styles( $styles = array() )
 }
 
 function enqueue_less_styles($tag, $handle) {
-    global $wp_styles;
-    $match_pattern = '/\.less$/U';
-    if ( preg_match( $match_pattern, $wp_styles->registered[$handle]->src ) ) {
-        $handle = $wp_styles->registered[$handle]->handle;
-        $media = $wp_styles->registered[$handle]->args;
-        $href = $wp_styles->registered[$handle]->src . '?ver=' . $wp_styles->registered[$handle]->ver;
-        $rel = isset($wp_styles->registered[$handle]->extra['alt']) && $wp_styles->registered[$handle]->extra['alt'] ? 'alternate stylesheet' : 'stylesheet';
-        $title = isset($wp_styles->registered[$handle]->extra['title']) ? "title='" . esc_attr( $wp_styles->registered[$handle]->extra['title'] ) . "'" : '';
-        $tag = "<link rel='stylesheet' id='$handle-css' $title href='$href' type='text/less' media='$media' />\n";
-        $tag = preg_replace('/\s+/', ' ', $tag);
-    }
-    return $tag;
+	global $wp_styles;
+	$match_pattern = '/\.less$/U';
+	if ( preg_match( $match_pattern, $wp_styles->registered[$handle]->src ) ) {
+		$handle = $wp_styles->registered[$handle]->handle;
+		$media = $wp_styles->registered[$handle]->args;
+		$href = $wp_styles->registered[$handle]->src . '?ver=' . $wp_styles->registered[$handle]->ver;
+		$rel = isset($wp_styles->registered[$handle]->extra['alt']) && $wp_styles->registered[$handle]->extra['alt'] ? 'alternate stylesheet' : 'stylesheet';
+		$title = isset($wp_styles->registered[$handle]->extra['title']) ? "title='" . esc_attr( $wp_styles->registered[$handle]->extra['title'] ) . "'" : '';
+		$tag = "<link rel='stylesheet' id='$handle-css' $title href='$href' type='text/less' media='$media' />\n";
+		$tag = preg_replace('/\s+/', ' ', $tag);
+	}
+	return $tag;
 }
 add_filter('style_loader_tag', 'enqueue_less_styles', 5, 2);
 
