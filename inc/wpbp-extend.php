10dea6610162277a57b62e18276c51840a028625
Jonathan Roy <jroy@optimumweb.ca>
2012-02-11 15:33:18 -0500

    Fix minimal menu walker

--- inc/wpbp-extend.php
+++ inc/wpbp-extend.php
@@ -258,14 +258,14 @@ class Minimal_Walker extends Walker_Nav_Menu
         $classes = empty( $item->classes ) ? array() : (array) $item->classes;
         
         $class_names = join( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item ) );
-        $class_names = ' class="'. esc_attr( $class_names ) . '"';
+        $class_names = ' class="' . esc_attr( $class_names ) . '"';
         
-        $attributes  = ! empty( $item->attr_title ) ? ' title="'  . esc_attr( $item->attr_title ) .'"' : '';
-        $attributes .= ! empty( $item->target )     ? ' target="' . esc_attr( $item->target     ) .'"' : '';
-        $attributes .= ! empty( $item->xfn )        ? ' rel="'    . esc_attr( $item->xfn        ) .'"' : '';
-        $attributes .= ! empty( $item->url )        ? ' href="'   . esc_attr( $item->url        ) .'"' : '';
+        $attributes  = ! empty( $item->attr_title ) ? ' title="'  . esc_attr( $item->attr_title ) . '"' : '';
+        $attributes .= ! empty( $item->target )     ? ' target="' . esc_attr( $item->target     ) . '"' : '';
+        $attributes .= ! empty( $item->xfn )        ? ' rel="'    . esc_attr( $item->xfn        ) . '"' : '';
+        $attributes .= ! empty( $item->url )        ? ' href="'   . esc_attr( $item->url        ) . '"' : '';
         
-        $item_output .= '<a'. $class_names .'" '. $attributes .'>';
+        $item_output .= '<a' . $class_names . ' ' . $attributes . '>';
         $item_output .= $args->link_before;
         $item_output .= apply_filters( 'the_title', $item->title, $item->ID );
         $item_output .= $args->link_after;
