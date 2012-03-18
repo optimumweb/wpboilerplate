c4ff66861b0db932db129aaef8c6f350415a3fdb
Jonathan Roy <jroy@optimumweb.ca>
2012-03-17 20:12:11 -0400

    Clean code

--- footer.php
+++ footer.php
@@ -1,15 +1,15 @@
-	<?php global $wpbp_options; ?>
+<?php global $wpbp_options; ?>
 		<?php wpbp_footer_before(); ?>
 		<footer id="footer" role="contentinfo">
 			<?php wpbp_footer_inside(); ?>
 			<div class="container <?php echo $wpbp_options['container_class']; ?>">
-                <div id="footer-nav">
-                    <?php wp_nav_menu( array( 'theme_location' => 'secondary_navigation' ) ); ?>
-            		<div class="clear"></div>
-                </div>
+				<div id="footer-nav">
+					<?php wp_nav_menu( array( 'theme_location' => 'secondary_navigation' ) ); ?>
+					<div class="clear"></div>
+				</div>
 				<div id="copy">
-                    <small>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></small>
-                </div>
+					<small>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></small>
+				</div>
 				<div class="clear"></div>
 			</div>
 		</footer>
