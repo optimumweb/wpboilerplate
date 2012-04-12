92bd082eee3b21ced6d46a3bef3abce92c780f33
Jonathan Roy <jroy@optimumweb.ca>
2012-04-12 13:54:05 -0400

    Clean

--- inc/wpbp-shortcodes.php
+++ inc/wpbp-shortcodes.php
@@ -1,40 +1,40 @@
 <?php
 
 // [container cols="12"]...[/container]
-function container_960gs($atts, $content = null) {
+function container_960gs($atts, $content = null)
+{
 	extract(shortcode_atts(array(
-		"cols" => "16"
+		'cols' => '16'
 	), $atts));
-	return "<div class=\"container_" . $cols . "\">" . parse_shortcode_content($content) . "</div>";
+	return '<div class="container_' . $cols . '">' . parse_shortcode_content($content) . '</div>';
 }
-
-add_shortcode("container", "container_960gs");
+add_shortcode('container', 'container_960gs');
 
 // [grid cols="6"]...[/grid]
-function grid_960gs($atts, $content = null) {
+function grid_960gs($atts, $content = null)
+{
 	extract(shortcode_atts(array(
-		"cols" => "1"
+		'cols' => '1'
 	), $atts));
-	return "<div class=\"grid_" . $cols . "\">" . parse_shortcode_content($content) . "</div>";
+	return '<div class="grid_' . $cols . '">' . parse_shortcode_content($content) . '</div>';
 }
-
-add_shortcode("grid", "grid_960gs");
+add_shortcode('grid', 'grid_960gs');
 
 // [clear]
-function clear_960gs() {
-	return "<div class=\"clear\"></div>";
+function clear_960gs()
+{
+	return '<div class="clear"></div>';
 }
-
-add_shortcode("clear", "clear_960gs");
+add_shortcode('clear', 'clear_960gs');
 
 // [hr]
-function wpbp_hr() {
-	return "<hr />";
+function wpbp_hr()
+{
+	return '<hr />';
 }
+add_shortcode('hr', 'wpbp_hr');
 
-add_shortcode("hr", "wpbp_hr");
-
-/*
+/**
  * MAKE BOX
  */
 
@@ -67,18 +67,18 @@ function make_box($atts, $content = null)
     if ( $src ) $data .= ' data-src="' . $src . '"';
     
     
-    $box = '<div' . $id . $class . $data . '>' . PHP_EOL;
+    $box = '<div' . $id . $class . $data . '>';
     
 	if ( isset($title) && strlen($title) > 0 ) {
 		$box .= '<div class="box-title title"><h3>';
 		if ( $ajax && $src ) $box .= '<a class="ajax-trigger" href="' . $src . '">' . $title . '</a>';
 		else $box .= $title;
-		$box .= '</h3></div>' . PHP_EOL;
+		$box .= '</h3></div>';
 	}
 		
-	$box .= '<div class="box-content content">' . PHP_EOL;
-	$box .= parse_shortcode_content($content) . PHP_EOL;
-	$box .= '<div class="clear"></div></div>' . PHP_EOL;
+	$box .= '<div class="box-content content">';
+	$box .= parse_shortcode_content($content);
+	$box .= '<div class="clear"></div></div>';
     
     $box_arrow_src = ( $small ) ? 'box-arrow-small.png' : 'box-arrow.png';
     $box_arrow = wpbp_get_image_tag( array( 'src' => 'http://pierreroy.firecdn.net/img/' . $box_arrow_src, 'width' => 30, 'height' => 30 ) );
@@ -87,33 +87,35 @@ function make_box($atts, $content = null)
     	$box .= '<div class="box-controls">';
     	$box .= '<a class="box-arrow box-prev" href="#prev">' . $box_arrow . '</a>';
     	$box .= '<a class="box-arrow box-next" href="#next">' . $box_arrow . '</a>';
-    	$box .= '</div>' . PHP_EOL;
+    	$box .= '</div>';
     }
     
     if ( $collapsible ) {
     	$box .= '<div class="box-controls">';
     	$box .= '<a class="box-arrow collapse-trigger valign" data-ref="#' . $id_val . ' .box-title" href="#">' . $box_arrow . '</a>';
-    	$box .= '</div>' . PHP_EOL;
+    	$box .= '</div>';
     }
     
-    $box .= '<div class="clear"></div>' . PHP_EOL;
-    $box .= '</div>' . PHP_EOL;
+    $box .= '<div class="clear"></div>';
+    $box .= '</div>';
     
     return $box;
 }
 add_shortcode('box', 'make_box');
 
 // [article-header]...[/article-header]
-function wpbp_article_header($atts = null, $content = null) {
-	return "<div class=\"article-header\">" . parse_shortcode_content($content) . "</div>";
+function wpbp_article_header($atts = null, $content = null)
+{
+	return '<div class="article-header">' . parse_shortcode_content($content) . '</div>';
 }
-
 add_shortcode("article-header", "wpbp_article_header");
 
-// Clean up shortcode $content
+/**
+ * Clean up shortcode $content
+ */
 
-function parse_shortcode_content($content) {
-
+function parse_shortcode_content($content)
+{
 	/* Parse nested shortcodes and add formatting. */
 	$content = trim( wpautop( do_shortcode( $content ) ) );
 
@@ -133,13 +135,13 @@ function parse_shortcode_content($content) {
 	return $content;
 }
 
-/*
-Plugin Name: Show Menu Shortcode
-Plugin URI: http://www.mokamedianyc.com/dev/show-menu-shortcode/
-Description: Provides a [show-menu] <a href="http://codex.wordpress.org/Shortcode_API">shortcodes</a> for displaying a menu within a post or page.  The shortcode accepts most parameters that you can pass to the <a href="http://codex.wordpress.org/Template_Tags/wp_nav_menu">wp_nav_menu()</a> function.  To show a menu, add [show-menu menu="Main-menu"] in the page or post body.
-Author: Bob Matsuoka
-Version: 1.0
-Author URI: http://www.mokamedianyc.com
+/**
+ * Plugin Name: Show Menu Shortcode
+ * Plugin URI: http://www.mokamedianyc.com/dev/show-menu-shortcode/
+ * Description: Provides a [show-menu] <a href="http://codex.wordpress.org/Shortcode_API">shortcodes</a> for displaying a menu within a post or page.  The shortcode accepts most parameters that you can pass to the <a href="http://codex.wordpress.org/Template_Tags/wp_nav_menu">wp_nav_menu()</a> function.  To show a menu, add [show-menu menu="Main-menu"] in the page or post body.
+ * Author: Bob Matsuoka
+ * Version: 1.0
+ * Author URI: http://www.mokamedianyc.com
 */
 
 function shortcode_show_menu($atts, $content, $tag)
@@ -214,5 +216,4 @@ function make_paypal($atts, $content = null) {
     $paypal = ob_get_clean();
     return parse_shortcode_content($paypal);
 }
-
-add_shortcode("paypal", "make_paypal");
+add_shortcode('paypal', 'make_paypal');
