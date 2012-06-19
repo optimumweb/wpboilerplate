2293f119d2888a4d28279a15cf1f451b508540bf
Jonathan Roy <jroy@optimumweb.ca>
2012-06-19 14:32:21 -0400

    Try to use built-in wp function first

--- inc/wpbp-extend.php
+++ inc/wpbp-extend.php
@@ -127,20 +127,13 @@ if ( !function_exists('featured_image') ) {
 
 }
 
-function wpbp_get_the_excerpt($post_ID = null, $limit = 250) { return null; }
-
 if ( !function_exists('wpbp_get_the_excerpt') ) {
 
 	function wpbp_get_the_excerpt($post_ID = null, $limit = 250)
 	{
         set_post_ID($post_ID);
-        if ( isset( get_the_excerpt($post_ID) ) && strlen( get_the_excerpt($post_ID) ) > 0 ) {
-        	$excerpt = get_the_excerpt($post_ID);
-        }
-        else {
-			$post = get_post($post_ID);
-			$excerpt = ( isset( $post->post_excerpt ) && strlen( $post->post_excerpt ) > 0 ) ? $post->post_excerpt : substr( strip_tags( $post->post_content ), 0, 250 ) . '...';
-		}
+		$post = get_post($post_ID);
+		$excerpt = ( isset( $post->post_excerpt ) && strlen( $post->post_excerpt ) > 0 ) ? $post->post_excerpt : substr( strip_tags( $post->post_content ), 0, 250 ) . '...';
 		return $excerpt;
 	}
 
