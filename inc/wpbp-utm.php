3f8ca8299e3a59abf00f7e8631da5d6a41f764ee
Jonathan Roy <jroy@optimumweb.ca>
2012-07-09 11:35:49 -0400

    Parse all utm but some will be empty

--- inc/wpbp-utm.php
+++ inc/wpbp-utm.php
@@ -50,24 +50,10 @@ function wpbp_get_utm($key = null, $override = array())
 function wpbp_parse_utm_fields($utm = array())
 {
 ?>
-	<?php if ( isset( $utm['utmccn'] ) ) : ?>
-		<input id="utmccn" name="field[utmcn][value]" value="<?php echo $utm['utmccn']; ?>" type="hidden" />
-	<?php endif; ?>
-	
-	<?php if ( isset( $utm['utmcmd'] ) ) : ?>
-		<input id="utmcmd" name="field[utmcmd][value]" value="<?php echo $utm['utmcmd']; ?>" type="hidden" />
-	<?php endif; ?>
-	
-	<?php if ( isset( $utm['utmcsr'] ) ) : ?>
-		<input id="utmcsr" name="field[utmcsr][value]" value="<?php echo $utm['utmcsr']; ?>" type="hidden" />
-	<?php endif; ?>
-	
-	<?php if ( isset( $utm['utmctr'] ) ) : ?>
-		<input id="utmctr" name="field[utmctr][value]" value="<?php echo $utm['utmctr']; ?>" type="hidden" />
-	<?php endif; ?>
-	
-	<?php if ( isset( $utm['utmcct'] ) ) : ?>
-		<input id="utmcct" name="field[utmcct][value]" value="<?php echo $utm['utmcct']; ?>" type="hidden" />
-	<?php endif; ?>
+	<input id="utmccn" name="field[utmccn][value]" value="<?php echo $utm['utmccn']; ?>" type="hidden" />
+	<input id="utmcmd" name="field[utmcmd][value]" value="<?php echo $utm['utmcmd']; ?>" type="hidden" />
+	<input id="utmcsr" name="field[utmcsr][value]" value="<?php echo $utm['utmcsr']; ?>" type="hidden" />
+	<input id="utmctr" name="field[utmctr][value]" value="<?php echo $utm['utmctr']; ?>" type="hidden" />
+	<input id="utmcct" name="field[utmcct][value]" value="<?php echo $utm['utmcct']; ?>" type="hidden" />
 <?php
 }
