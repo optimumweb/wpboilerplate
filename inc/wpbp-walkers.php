ee7f1d8df3ff3e4860ace983070eb5b1f4b14047
Jonathan Roy <jroy@optimumweb.ca>
2012-08-28 03:31:36 -0400

    Add capability-based nav walker

--- inc/wpbp-walkers.php
+++ inc/wpbp-walkers.php
@@ -84,46 +84,39 @@ class Capability_Based_Walker extends Walker_Nav_Menu
 {
 	function start_el(&$output, $item, $depth, $args = array())
 	{
-		if ( strlen( $item->xfn ) >= 1 && !current_user_can( $item->xfn ) ) {
-			$output .= "";
-		}
-		else {
+		if ( strlen( $item->xfn ) >= 1 && !current_user_can( $item->xfn ) ) return;
 
-			global $wp_query;
+		global $wp_query;
 
-			$indent = ( $depth ) ? str_repeat( "\t", $depth ) : '';
+		$indent = ( $depth ) ? str_repeat( "\t", $depth ) : '';
 
-			$class_names = $value = '';
+		$class_names = $value = '';
 
-			$classes = empty( $item->classes ) ? array() : (array) $item->classes;
+		$classes = empty( $item->classes ) ? array() : (array) $item->classes;
 
-			$class_names = join( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item ) );
-			$class_names = ' class="'. esc_attr( $class_names ) . '"';
+		$class_names = join( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item ) );
+		$class_names = ' class="'. esc_attr( $class_names ) . '"';
 
-			$output .= $indent . '<li id="menu-item-'. $item->ID . '"' . $value . $class_names .'>';
+		$output .= $indent . '<li id="menu-item-'. $item->ID . '"' . $value . $class_names .'>';
 
-			$attributes  = ! empty( $item->attr_title ) ? ' title="'  . esc_attr( $item->attr_title ) . '"' : '';
-			$attributes .= ! empty( $item->target )     ? ' target="' . esc_attr( $item->target )     . '"' : '';
-			$attributes .= ! empty( $item->url )        ? ' href="'   . esc_attr( $item->url )        . '"' : '';
+		$attributes  = ! empty( $item->attr_title ) ? ' title="'  . esc_attr( $item->attr_title ) . '"' : '';
+		$attributes .= ! empty( $item->target )     ? ' target="' . esc_attr( $item->target )     . '"' : '';
+		$attributes .= ! empty( $item->url )        ? ' href="'   . esc_attr( $item->url )        . '"' : '';
 
-			$item_output = $args->before;
-			$item_output .= '<a'. $attributes .'>';
-			$item_output .= $args->link_before . apply_filters( 'the_title', $item->title, $item->ID );
-			$item_output .= $args->link_after;
-			$item_output .= '</a>';
-			$item_output .= $args->after;
+		$item_output = $args->before;
+		$item_output .= '<a'. $attributes .'>';
+		$item_output .= $args->link_before . apply_filters( 'the_title', $item->title, $item->ID );
+		$item_output .= $args->link_after;
+		$item_output .= '</a>';
+		$item_output .= $args->after;
 
-			$output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
-		}
+		$output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
 	}
 
 	function end_el(&$output, $item, $depth, $args = array())
 	{
-		if ( strlen( $item->xfn ) >= 1 && !current_user_can( $item->xfn ) ) {
-			$output .= "";
-		}
-		else {
-			$output .= "</li>\n";
-		}
+		if ( strlen( $item->xfn ) >= 1 && !current_user_can( $item->xfn ) ) return;
+
+		$output .= "</li>\n";
 	}
 }
