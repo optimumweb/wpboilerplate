c8310d8dd99e5d593d1f046c6911a7a132bbe919
Jonathan Roy <jroy@optimumweb.ca>
2012-10-27 14:52:05 -0400

    Re-structure

--- inc/wpbp-enqueue.php
+++ inc/wpbp-enqueue.php
@@ -1,124 +1,12 @@
 <?php
 
-$wpbp_libs = array(
-  'jquery' => array(
-    'js'   => TEMPLATE_URI . '/libs/jquery/jquery.min.js',
-    'ver'  => '1.7.2'
-  ),
-  'jquery-ui' => array(
-    'js'   => TEMPLATE_URI . '/libs/jquery-ui/jquery-ui.min.js',
-    'css'  => TEMPLATE_URI . '/libs/jquery-ui/css/base/jquery.ui.all.css',
-    'ver'  => '1.8.22'
-  ),
-  'jquery-ui-smoothness' => array(
-    'css'  => TEMPLATE_URI . '/libs/jquery-ui/css/smoothness/jquery-ui.css'
-  ),
-  'jquery-ui-lightness' => array(
-    'css'  => TEMPLATE_URI . '/libs/jquery-ui/css/ui-lightness/jquery-ui.css'
-  ),
-  'jquery-ui-darkness' => array(
-    'css'  => TEMPLATE_URI . '/libs/jquery-ui/css/ui-darkness/jquery-ui.css'
-  ),
-  'jquery-ui-redmond' => array(
-    'css'  => TEMPLATE_URI . '/libs/jquery-ui/css/redmond/jquery-ui.css'
-  ),
-  'jquery-ui-blitzer' => array(
-    'css'  => TEMPLATE_URI . '/libs/jquery-ui/css/blitzer/jquery-ui.css'
-  ),
-  '960gs' => array(
-    'css'  => TEMPLATE_URI . '/libs/960gs/960.min.css'
-  ),
-  'scrollTo' => array(
-    'js'   => TEMPLATE_URI . '/libs/scrollTo/jquery.scrollTo.min.js',
-    'deps' => array('jquery'),
-    'ver'  => '1.4.2'
-  ),
-  'cycle' => array(
-    'js'   => TEMPLATE_URI . '/libs/cycle/jquery.cycle.min.js',
-    'deps' => array('jquery'),
-    'ver'  => '2.9998'
-  ),
-  'powerslide' => array(
-    'js'   => TEMPLATE_URI . '/libs/powerslide/js/powerslide.min.js',
-    'css'  => TEMPLATE_URI . '/libs/powerslide/css/powerslide.css',
-    'deps' => array('jquery'),
-    'ver'  => '1.1'
-  ),
-  'lightbox' => array(
-    'js'   => TEMPLATE_URI . '/libs/lightbox/js/lightbox.min.js',
-    'css'  => TEMPLATE_URI . '/libs/lightbox/css/lightbox.min.css',
-    'deps' => array('jquery'),
-    'ver'  => '2.51'
-  ),
-  'hoverIntent' => array(
-    'js'   => TEMPLATE_URI . '/libs/hoverIntent/jquery.hoverIntent.min.js',
-    'deps' => array('jquery'),
-    'ver'  => '6.0'
-  ),
-  'flexslider' => array(
-    'js'   => TEMPLATE_URI . '/libs/flexslider/jquery.flexslider.min.js',
-    'css'  => TEMPLATE_URI . '/libs/flexslider/flexslider.css',
-    'deps' => array('jquery'),
-    'ver'  => '2.1'
-  ),
-  'bootstrap' => array(
-    'js'   => TEMPLATE_URI . '/libs/bootstrap/js/bootstrap.min.js',
-    'css'  => TEMPLATE_URI . '/libs/bootstrap/css/bootstrap.min.css',
-    'deps' => array('jquery'),
-    'ver'  => '2.0.4'
-  ),
-  'bootstrap-responsive' => array(
-    'css'  => TEMPLATE_URI . '/libs/bootstrap/css/bootstrap-responsive.min.css'
-  ),
-  'ext-core' => array(
-    'js'   => 'https://ajax.googleapis.com/ajax/libs/ext-core/3.1.0/ext-core.js',
-    'ver'  => '3.1.0'
-  ),
-  'dojo' => array(
-    'js'   => 'https://ajax.googleapis.com/ajax/libs/dojo/1.6.1/dojo/dojo.xd.js',
-    'ver'  => '1.6.1'
-  ),
-  'mootools' => array(
-    'js'   => 'https://ajax.googleapis.com/ajax/libs/mootools/1.4.1/mootools-yui-compressed.js',
-    'ver'  => '1.4.1'
-  ),
-  'modernizr' => array(
-    'js'   => TEMPLATE_URI . '/libs/modernizr/modernizr.min.js',
-    'ver'  => '2.6.2'
-  ),
-  'lesscss' => array(
-    'js'   => TEMPLATE_URI . '/libs/less/less.min.js',
-    'ver'  => '1.2.1'
-  ),
-  'sizzle' => array(
-    'js'   => TEMPLATE_URI . '/libs/sizzle/sizzle.min.js',
-    'ver'  => '1.5.1'
-  ),
-  'highcharts' => array(
-    'js'   => TEMPLATE_URI . '/libs/highcharts/highcharts.min.js',
-    'ver'  => '2.1.9'
-  ),
-  'default' => array(
-    'css'  => TEMPLATE_URI . '/libs/wpbp/css/default.css'
-  ),
-  'wpbp' => array(
-    'js'   => TEMPLATE_URI . '/js/wpbp.min.js',
-    'css'  => TEMPLATE_URI . '/css/wpbp.css',
-    'deps' => array('jquery'),
-    'ver'  => '3.1.0'
-  ),
-  'wp-meta' => array(
-    'css'  => THEME_URI . '/style.css'
-  )
-);
-
 function wpbp_register_libs()
 {
 	if ( is_admin() ) return;
 
-	global $wpbp_libs;
+	$wpbp_lib = wpbp_get_lib();
 
-  foreach ( $wpbp_libs as $handle => $lib ) {
+  foreach ( $wpbp_lib as $handle => $lib ) {
     if ( wpbp_is_valid_var( $lib['js'] ) ) {
       wpbp_register_script($handle, $lib['js'], $lib['deps'], $lib['ver'], $lib['in_footer']);
     }
