e12a3883e57d3822aa40c82c263055ee5fa35b20
Jonathan Roy <jroy@optimumweb.ca>
2013-01-05 13:05:15 -0500

    Use hgroup instead of section for a more semantic outline

--- header.php
+++ header.php
@@ -5,10 +5,10 @@
 		<header id="header" role="banner">
 			<?php wpbp_header_inside_before(); ?>
             <div class="container <?php wpbp_option('container_class'); ?>">
-            	<section id="site-title">
+            	<hgroup id="site-title">
 					<h1><a href="<?php echo home_url(); ?>/"><?php bloginfo('name'); ?></a></h1>
 					<h3><?php bloginfo('description'); ?></h3>
-			    </section>
+			    </hgroup>
 			    <nav id="main-nav" role="navigation">
 			    	<div class="container">
 				    	<?php wp_nav_menu( array( 'theme_location' => 'primary_navigation' ) ); ?>
