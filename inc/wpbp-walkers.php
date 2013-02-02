0d07efea02a025bf1add0048e4b7b77bca4083fd
Jonathan Roy <jroy@optimumweb.ca>
2013-02-02 02:37:29 -0500

    Add Sister Nav Walker

--- inc/wpbp-walkers.php
+++ inc/wpbp-walkers.php
@@ -123,3 +123,66 @@ class Capability_Based_Walker extends Walker_Nav_Menu
 		$output .= "</li>\n";
 	}
 }
+
+class Sister_Nav_Walker extends Walker_Nav_Menu {
+    var $found_parents = array();
+    var $found_children = array();
+
+    function start_el(&$output, $item, $depth, $args) {
+        global $wp_query;
+
+        // this only works for second level sub navigations
+        $parent_item_id = 0;
+
+        $class_names = $value = '';
+
+        $classes = empty($item->classes) ? array() : (array) $item->classes;
+
+        $class_names = join(' ', apply_filters('nav_menu_css_class', array_filter( $classes ), $item));
+        $class_names = ' class="' . esc_attr($class_names) . '"';
+
+        if ( $item->menu_item_parent == 0 && strpos($class_names, 'current-menu-parent') ) {
+            $output.= "<li>";
+        }
+
+        // checks if the current element is in the current selection
+        if ( strpos($class_names, 'current-menu-item') || strpos($class_names, 'current-menu-parent') || strpos($class_names, 'current-menu-ancestor') || ( is_array($this->found_parents) && in_array($item->menu_item_parent, $this->found_parents) ) ) {
+
+            // keep track of all selected parents
+            $this->found_parents[] = $item->ID;
+
+            $output .= '<li id="menu-item-'. $item->ID . '"' . $value . $class_names .'>';
+
+            $attributes  = ! empty($item->attr_title) ? ' title="'  . esc_attr($item->attr_title) .'"' : '';
+            $attributes .= ! empty($item->target)     ? ' target="' . esc_attr($item->target    ) .'"' : '';
+            $attributes .= ! empty($item->xfn)        ? ' rel="'    . esc_attr($item->xfn       ) .'"' : '';
+            $attributes .= ! empty($item->url)        ? ' href="'   . esc_attr($item->url       ) .'"' : '';
+
+            $item_output = $args->before;
+            $item_output .= '<a'. $attributes .'>';
+            $item_output .= $args->link_before . apply_filters('the_title', $item->title, $item->ID) . $args->link_after;
+            $item_output .= '</a>';
+            $item_output .= $args->after;
+
+            $output .= apply_filters('walker_nav_menu_start_el', $item_output, $item, $depth, $args);
+
+        }
+    }
+
+    function end_el(&$output, $item, $depth) {
+        // closes only the opened li
+        if ( is_array($this->found_parents) && in_array($item->ID, $this->found_parents) ) {
+            $output .= "</li>";
+        }
+    }
+
+    function end_lvl(&$output, $depth) {
+        $output = trim($output);
+        // if the sub-menu is empty, strip the opening tag, else closes it
+        if ( substr($output, -21) == '<ul class="sub-menu">' ) {
+            $output = substr($output, 0, strlen($output)-21);
+        } else {
+            $output .= "</ul>";
+        }
+    }
+}
