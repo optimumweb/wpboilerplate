83a40b0f388500adb9e0369d4d954e84d8a908c4
Jonathan Roy <jroy@optimumweb.ca>
2013-04-05 18:22:57 -0400

    Add auto id generation

--- inc/wpbp-shortcodes.php
+++ inc/wpbp-shortcodes.php
@@ -97,7 +97,7 @@ function make_smartbox($atts, $content = null)
         'lazy'        => false
     ), $atts ) );
 
-    if ( !$id ) $id = sanitize_title_with_dashes($title);
+    if ( !$id ) $id = substr(sanitize_title($title), 0, 50);
 
     // define box #id
     $id = ' id="' . $id . '"';
