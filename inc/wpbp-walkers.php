bfb2abfc620ce829e2aee327a367b54375d5ca87
Jonathan Roy <jroy@optimumweb.ca>
2013-07-22 10:53:19 -0400

    Add space between title and description

--- inc/wpbp-walkers.php
+++ inc/wpbp-walkers.php
@@ -21,12 +21,14 @@ class Description_Walker extends Walker_Nav_Menu
         $attributes .= ! empty( $item->xfn )        ? ' rel="'    . esc_attr( $item->xfn )        . '"' : '';
         $attributes .= ! empty( $item->url )        ? ' href="'   . esc_attr( $item->url )        . '"' : '';
 
-        $description  = ! empty( $item->description ) ? ' <span>'.esc_attr( $item->description ).'</span>' : '';
+        $separator = '<span class="separator"> - </span>';
+
+        $description = ! empty( $item->description ) ? '<span class="description">'.esc_attr( $item->description ).'</span>' : '';
 
         $item_output = $args->before;
-        $item_output .= '<a'. $attributes .'>';
+        $item_output .= '<a' . $attributes . '>';
         $item_output .= $args->link_before . apply_filters( 'the_title', $item->title, $item->ID );
-        $item_output .= $description.$args->link_after;
+        $item_output .= $separator . $description . $args->link_after;
         $item_output .= '</a>';
         $item_output .= $args->after;
 
