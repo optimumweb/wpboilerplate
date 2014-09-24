296434a5d6a9e8c69066e775d7b97e8aa268b394
Jonathan Roy <jroy@optimumweb.ca>
2014-09-23 23:06:58 -0400

    Style

--- footer.php
+++ footer.php
@@ -2,13 +2,17 @@
 		<footer id="footer" role="contentinfo">
 			<?php wpbp_footer_inside_before(); ?>
 			<div class="container <?php wpbp_option('container_class'); ?>">
-				<nav id="footer-nav">
-					<?php wp_nav_menu( array( 'theme_location' => 'secondary_navigation' ) ); ?>
-					<div class="clear"></div>
-				</nav>
-				<div id="copy">
-					&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>
-				</div>
+                <div class="grid_8">
+                    <nav id="footer-nav">
+                        <?php wp_nav_menu( array( 'theme_location' => 'secondary_navigation' ) ); ?>
+                        <div class="clear"></div>
+                    </nav>
+                </div>
+                <div class="grid_4">
+                    <div id="copy">
+                        &copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>
+                    </div>
+                </div>
 			</div>
 			<?php wpbp_footer_inside_after(); ?>
 		</footer>
--- header.php
+++ header.php
@@ -5,16 +5,20 @@
 		<header id="header" role="banner">
 			<?php wpbp_header_inside_before(); ?>
             <div class="container <?php wpbp_option('container_class'); ?>">
-            	<hgroup id="site-title">
-					<h1><a href="<?php echo home_url(); ?>/"><?php bloginfo('name'); ?></a></h1>
-					<h3><?php bloginfo('description'); ?></h3>
-			    </hgroup>
-			    <nav id="main-nav" role="navigation">
-			    	<div class="container">
-				    	<?php wp_nav_menu( array( 'theme_location' => 'primary_navigation' ) ); ?>
-				    	<div class="clear"></div>
-			    	</div>
-			    </nav>
+                <div class="grid_4">
+                    <hgroup id="site-title">
+                        <h1><a href="<?php echo home_url(); ?>/"><?php bloginfo('name'); ?></a></h1>
+                        <h3><?php bloginfo('description'); ?></h3>
+                    </hgroup>
+                </div>
+                <div class="grid_8">
+                    <nav id="main-nav" role="navigation">
+                        <div class="container">
+                            <?php wp_nav_menu( array( 'theme_location' => 'primary_navigation' ) ); ?>
+                            <div class="clear"></div>
+                        </div>
+                    </nav>
+                </div>
             </div>
             <?php wpbp_header_inside_after(); ?>
 		</header>
