877c2ba591efed0e564e90c706470043baeaefdb
Jonathan Roy <jroy@optimumweb.ca>
2014-10-15 19:07:57 -0400

    Add new page template

--- page-wide.php
+++ page-wide.php
@@ -1,10 +1,11 @@
 <?php
 /*
-Template Name: Full Width
+Template Name: Wide
 */
 get_header(); ?>
-    <?php wpbp_content_before(); ?>
-	<section id="content">
+<?php wpbp_content_before(); ?>
+<section id="content">
+    <div class="container <?php wpbp_option('container_class'); ?>">
         <?php wpbp_main_before(); ?>
         <section id="main" role="main">
             <?php wpbp_main_inside_before(); ?>
@@ -16,6 +17,7 @@ get_header(); ?>
             <?php wpbp_main_inside_after(); ?>
         </section>
         <?php wpbp_main_after(); ?>
-	</section>
-	<?php wpbp_content_after(); ?>
+    </div>
+</section>
+<?php wpbp_content_after(); ?>
 <?php get_footer(); ?>
