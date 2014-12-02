c7e62995a49ea438b15c91b91828246ddd943258
Jonathan Roy <jroy@optimumweb.ca>
2014-12-02 15:51:44 -0500

    Update Google Analytics tracking code

--- inc/wpbp-actions.php
+++ inc/wpbp-actions.php
@@ -1,31 +1,29 @@
 <?php
 
-add_action('init', 'wpbp_register_lib');
-add_action('wpbp_head', 'wpbp_google_analytics_content_experiment');
-add_action('wpbp_head', 'wpbp_google_analytics');
-add_action('wpbp_head', 'wpbp_google_tag_manager');
-add_action('wpbp_head', 'wpbp_custom_css');
-add_action('wpbp_head', 'wpbp_favicon');
-add_action('wpbp_footer', 'wpbp_custom_js');
-add_action('wpbp_footer', 'wpbp_add_post_js');
+add_action('init',            'wpbp_register_lib');
+add_action('wpbp_head',       'wpbp_google_analytics_content_experiment');
+add_action('wpbp_head',       'wpbp_google_analytics');
+add_action('wpbp_head',       'wpbp_google_tag_manager');
+add_action('wpbp_head',       'wpbp_custom_css');
+add_action('wpbp_head',       'wpbp_favicon');
+add_action('wpbp_footer',     'wpbp_custom_js');
+add_action('wpbp_footer',     'wpbp_add_post_js');
 add_action('wpbp_loop_after', 'wpbp_clear');
 
 function wpbp_google_analytics()
 {
     $id = wpbp_get_option('google_analytics_id');
-    if ( $id !== '' ) : ?>
-<!-- Google Analytics -->
-<script type="text/javascript">
-    var _gaq = _gaq || [];
-    _gaq.push(['_setAccount', '<?php echo $id; ?>']);
-    _gaq.push(['_trackPageview']);
-    (function() {
-        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-    })();
+    if ( $id ) : ?>
+<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', '<?php echo $id; ?>', 'auto');
+    ga('send', 'pageview');
+
 </script>
-<!-- End Google Analytics -->
 <?php endif;
 }
 
