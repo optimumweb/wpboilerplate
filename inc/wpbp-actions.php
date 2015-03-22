88b02063088ecaa0ffa2c9b57b4d8fde2929dec6
Jonathan Roy <jroy@optimumweb.ca>
2015-03-22 18:50:37 -0400

    Move tags to a sub-folder

--- inc/wpbp-actions.php
+++ inc/wpbp-actions.php
@@ -29,78 +29,51 @@ function wpbp_insert_google_analytics()
 {
     $ga_id = wpbp_get_option('google_analytics_id');
 
-    if ( !empty($ga_id) ) : ?>
-<script>
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+    $file = TEMPLATE_DIRECTORY . '/inc/tags/google_analytics.php';
 
-    ga('create', '<?php echo $ga_id; ?>', 'auto');
-    ga('send', 'pageview');
-
-</script>
-    <?php endif;
+    if ( !empty($ga_id) && file_exists($file) ) {
+        include($file);
+    }
 }
 
 function wpbp_insert_google_remarketing_tag()
 {
     $google_remarketing_id = wpbp_get_option('google_remarketing_id');
 
-    if ( !empty($google_remarketing_id) ) : ?>
-<!-- Google Code for Remarketing Tag -->
-<!--------------------------------------------------
-Remarketing tags may not be associated with personally identifiable information or placed on pages related to sensitive categories. See more information and instructions on how to setup the tag on: http://google.com/ads/remarketingsetup
---------------------------------------------------->
-<script type="text/javascript">
-    /* <![CDATA[ */
-    var google_conversion_id = <?php echo $google_remarketing_id; ?>;
-    var google_custom_params = window.google_tag_params;
-    var google_remarketing_only = true;
-    /* ]]> */
-</script>
-<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
-</script>
-<noscript>
-    <div style="display:inline;">
-        <img height="1" width="1" style="border-style:none;" alt="" src="//googleads.g.doubleclick.net/pagead/viewthroughconversion/<?php echo $google_remarketing_id; ?>/?value=0&amp;guid=ON&amp;script=0"/>
-    </div>
-</noscript>
-    <?php endif;
+    $file = TEMPLATE_DIRECTORY . '/inc/tags/google_remarketing.php';
+
+    if ( !empty($google_remarketing_id) && file_exists($file) ) {
+        include($file);
+    }
 }
 
 function wpbp_insert_google_tag_manager()
 {
     $gtm_id = esc_attr( wpbp_get_option('google_tag_manager_id') );
 
-    if ( !empty($gtm_id) ) : ?>
-<!-- Google Tag Manager -->
-<noscript><iframe src="//www.googletagmanager.com/ns.html?id=<?php echo $gtm_id; ?>" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','<?php echo $gtm_id; ?>');</script>
-<!-- End Google Tag Manager -->
-    <?php endif;
+    $file = TEMPLATE_DIRECTORY . '/inc/tags/google_tag_manager.php';
+
+    if ( !empty($gtm_id) && file_exists($file) ) {
+        include($file);
+    }
 }
 
 function wpbp_insert_custom_css()
 {
     $custom_css = wpbp_get_option('custom_css');
 
-    if ( !empty($custom_css) ) : ?>
-<style type="text/css">
-<?php echo $custom_css; ?>
-</style>
-    <?php endif;
+    if ( !empty($custom_css) ) {
+        echo '<style type="text/css">'.$custom_css.'</style>';
+    }
 }
 
 function wpbp_insert_custom_js()
 {
     $custom_js = wpbp_get_option('custom_js');
 
-    if ( !empty($custom_js) ) : ?>
-<script type="text/javascript">
-<?php echo $custom_js; ?>
-</script>
-    <?php endif;
+    if ( !empty($custom_js) ) {
+        echo '<script type="text/javascript">'.$custom_js.'</script>';
+    }
 }
 
 function wpbp_insert_post_js()
@@ -108,11 +81,9 @@ function wpbp_insert_post_js()
     set_post_ID($post_ID);
     $post_js = get_post_meta($post_ID, 'js', true);
 
-    if ( !empty($post_js) ) : ?>
-<script type="text/javascript">
-<?php echo $post_js; ?>
-</script>
-    <?php endif;
+    if ( !empty($post_js) ) {
+        echo '<script type="text/javascript">'.$post_js.'</script>';
+    }
 }
 
 function wpbp_insert_favicon()
