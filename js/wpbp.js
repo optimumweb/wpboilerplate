45d0825cf84ec11bded5f9690c65db49a5305449
Jonathan Roy <jroy@optimumweb.ca>
2016-06-29 15:59:36 -0400

    animate

--- js/wpbp.js
+++ js/wpbp.js
@@ -876,7 +876,7 @@
 
                     $cover.stop(true, true).animate({
                         'background-position-y': coverBgPosY + '%'
-                    }, 25, 'linear');
+                    }, 50);
 
                 }
 
