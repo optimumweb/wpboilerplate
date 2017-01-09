0af0e6106118ff5ebb474e5f8fbae37d499afe23
Jonathan Roy <jroy@optimumweb.ca>
2017-01-09 10:33:54 -0500

    fix templates

--- page-full.php
+++ page-full.php
@@ -3,17 +3,17 @@
 Template Name: Full Width
 */
 get_header(); ?>
-    <?php wpbp_content_before(); ?>
-	<section id="content">
-        <?php wpbp_main_before(); ?>
-        <section id="main" role="main">
-            <?php wpbp_main_inside_before(); ?>
-            <?php wpbp_loop_before(); ?>
-            <?php get_template_part('loop', 'page'); ?>
-            <?php wpbp_loop_after(); ?>
-            <?php wpbp_main_inside_after(); ?>
-        </section>
-        <?php wpbp_main_after(); ?>
-	</section>
-	<?php wpbp_content_after(); ?>
+<?php wpbp_content_before(); ?>
+<section id="content">
+    <?php wpbp_main_before(); ?>
+    <section id="main" role="main">
+        <?php wpbp_main_inside_before(); ?>
+        <?php wpbp_loop_before(); ?>
+        <?php get_template_part('loop', 'page'); ?>
+        <?php wpbp_loop_after(); ?>
+        <?php wpbp_main_inside_after(); ?>
+    </section>
+    <?php wpbp_main_after(); ?>
+</section>
+<?php wpbp_content_after(); ?>
 <?php get_footer(); ?>
--- page-wide.php
+++ page-wide.php
@@ -6,15 +6,19 @@ get_header(); ?>
 <?php wpbp_content_before(); ?>
 <section id="content">
     <div class="<?php wpbp_container_class(); ?>">
-        <?php wpbp_main_before(); ?>
-        <section id="main" role="main">
-            <?php wpbp_main_inside_before(); ?>
-            <?php wpbp_loop_before(); ?>
-            <?php get_template_part('loop', 'page'); ?>
-            <?php wpbp_loop_after(); ?>
-            <?php wpbp_main_inside_after(); ?>
-        </section>
-        <?php wpbp_main_after(); ?>
+        <div class="<?php wpbp_container_class(); ?>">
+            <div class="grid_12">
+                <?php wpbp_main_before(); ?>
+                <section id="main" role="main">
+                    <?php wpbp_main_inside_before(); ?>
+                    <?php wpbp_loop_before(); ?>
+                    <?php get_template_part('loop', 'page'); ?>
+                    <?php wpbp_loop_after(); ?>
+                    <?php wpbp_main_inside_after(); ?>
+                </section>
+                <?php wpbp_main_after(); ?>
+            </div>
+        </div>
     </div>
 </section>
 <?php wpbp_content_after(); ?>
