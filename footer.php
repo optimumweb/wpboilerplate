b4489301ad1fcd3ab67b6584a1fe69c95d6b0279
Jonathan Roy <jroy@optimumweb.ca>
2017-01-09 14:35:47 -0500

    style

--- footer.php
+++ footer.php
@@ -2,13 +2,13 @@
 		<footer id="footer" role="contentinfo">
 			<?php wpbp_footer_inside_before(); ?>
 			<div class="<?php wpbp_container_class(); ?>">
-                <div class="grid_8">
+                <div class="grid_8 mobile-center">
                     <nav id="footer-nav">
                         <?php wp_nav_menu(array( 'theme_location' => 'secondary_navigation' )); ?>
                         <div class="clear"></div>
                     </nav>
                 </div>
-                <div class="grid_4">
+                <div class="grid_4 text-right mobile-center">
                     <div id="copy">
                         &copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>
                     </div>
--- header.php
+++ header.php
@@ -5,13 +5,13 @@
 		<header id="header" role="banner">
 			<?php wpbp_header_inside_before(); ?>
             <div class="<?php wpbp_container_class(); ?>">
-                <div class="grid_4">
+                <div class="grid_4 mobile-center">
                     <hgroup id="site-title">
                         <h1><a href="<?php echo home_url(); ?>/"><?php bloginfo('name'); ?></a></h1>
                         <h3><?php bloginfo('description'); ?></h3>
                     </hgroup>
                 </div>
-                <div class="grid_8">
+                <div class="grid_8 text-right mobile-center">
                     <nav id="main-nav" role="navigation">
                         <?php wp_nav_menu(array( 'theme_location' => 'primary_navigation' )); ?>
                     </nav>
