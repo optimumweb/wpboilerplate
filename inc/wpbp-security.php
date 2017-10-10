316e925b630a9ac97d5c58f41e8292f94aa9c44f
Jonathan Roy <jroy@optimumweb.ca>
2017-10-10 15:18:02 -0400

    cleanup and add force_https option

--- inc/wpbp-security.php
+++ inc/wpbp-security.php
@@ -2,12 +2,9 @@
 
 function wpbp_force_https()
 {
-    if ( !is_ssl() ) {
-        wp_redirect('https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'], 301 );
+    if ( wpbp_get_option('force_https') == 'yes' && !is_ssl() ) {
+        wp_redirect('https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'], 301);
         exit();
     }
 }
-
-if ( wpbp_get_option('force_https') == 'yes' ) {
-    add_action('template_redirect', 'wpbp_force_https', 1);
-}
+add_action('template_redirect', 'wpbp_force_https', 1);
