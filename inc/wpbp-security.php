0225f58b101687225bfd19d5dbbf0246ebab2689
Jonathan Roy <jroy@optimumweb.ca>
2017-10-10 17:18:39 -0400

    add 'FORCE_SSL_ADMIN' to 'force_https' function/option

--- inc/wpbp-security.php
+++ inc/wpbp-security.php
@@ -2,9 +2,12 @@
 
 function wpbp_force_https()
 {
-    if ( wpbp_get_option('force_https') == 'yes' && !is_ssl() ) {
-        wp_redirect('https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'], 301);
-        exit();
+    if ( wpbp_get_option('force_https') == 'yes' ) {
+        if ( !defined('FORCE_SSL_ADMIN') ) define('FORCE_SSL_ADMIN', true);
+        if ( !is_ssl() ) {
+            wp_redirect('https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'], 301);
+            exit();
+        }
     }
 }
 add_action('template_redirect', 'wpbp_force_https', 1);
