2cdfbf3e2c9d9ea239d4ca009714a30c498ee5f1
Jonathan Roy <jroy@optimumweb.ca>
2017-10-10 17:21:34 -0400

    cannot define constant late

--- inc/wpbp-security.php
+++ inc/wpbp-security.php
@@ -3,7 +3,6 @@
 function wpbp_force_https()
 {
     if ( wpbp_get_option('force_https') == 'yes' ) {
-        if ( !defined('FORCE_SSL_ADMIN') ) define('FORCE_SSL_ADMIN', true);
         if ( !is_ssl() ) {
             wp_redirect('https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'], 301);
             exit();
