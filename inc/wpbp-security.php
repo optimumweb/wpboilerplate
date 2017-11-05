7aa2c9a6f633d3031e6ddee9c058b133814dcf9b
Jonathan Roy <jroy@optimumweb.ca>
2017-11-05 12:21:47 -0500

    support hsts max-age

--- inc/wpbp-security.php
+++ inc/wpbp-security.php
@@ -14,7 +14,7 @@ add_action('template_redirect', 'wpbp_force_https', 1);
 
 function wpbp_hsts_header()
 {
-    if ( wpbp_get_option('force_https') == 'yes' ) {
+    if ( is_ssl() ) {
         $hsts_max_age = wpbp_get_option('hsts_max_age');
         if ( $hsts_max_age != '' ) {
             header("Strict-Transport-Security: max-age=" . $hsts_max_age);
