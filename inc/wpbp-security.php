83498ffc46ef00513a21c62f77bd4b579f56acd5
Jonathan Roy <jroy@optimumweb.ca>
2017-11-06 20:27:32 -0500

    optimize

--- inc/wpbp-security.php
+++ inc/wpbp-security.php
@@ -2,11 +2,9 @@
 
 function wpbp_force_https()
 {
-    if ( wpbp_get_option('force_https') == 'yes' ) {
-        if ( !is_ssl() ) {
-            wp_redirect('https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'], 301);
-            exit();
-        }
+    if ( !is_ssl() && wpbp_get_option('force_https') == 'yes' ) {
+        wp_redirect('https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'], 301);
+        exit();
     }
 }
 add_action('template_redirect', 'wpbp_force_https', 1);
