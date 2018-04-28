3b488f91013ef4c927b7c68cd868e1a0b1a65969
Jonathan Roy <jroy@optimumweb.ca>
2018-04-28 11:16:04 -0400

    fix

--- inc/tags/google_analytics.php
+++ inc/tags/google_analytics.php
@@ -1,6 +1,6 @@
 <?php if ( !empty($ga_id) ) : ?>
 <!-- Global Site Tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-107875846-1"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=<?php echo $ga_id; ?>"></script>
 <script>
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
