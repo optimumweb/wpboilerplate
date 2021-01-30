6e37224cd368c30844d741f54d3fd39aee5c45ff
Jonathan Roy <jroy@optimumweb.ca>
2021-01-30 11:06:40 -0500

    debug

--- inc/wpbp-actions.php
+++ inc/wpbp-actions.php
@@ -61,7 +61,7 @@ function wpbp_insert_google_analytics() {
     $google_analytics_id = wpbp_get_option( 'google_analytics_id' );
     $google_ads_id = wpbp_get_option( 'google_ads_id' );
 
-    if ( ! empty( $google_analytics_id ) || ! empty( $google_ads_id ) ) {
+    if ( ! empty( $google_analytics_id ) ) {
         $file = TEMPLATE_DIRECTORY . '/inc/tags/google_analytics.php';
 
         if ( file_exists( $file ) ) {
