15d40e1e825c4e320756ec2a324709ef048b625e
Jonathan Roy <jroy@optimumweb.ca>
2021-03-12 12:20:27 -0500

    remove debug

--- inc/tags/google_analytics.php
+++ inc/tags/google_analytics.php
@@ -1,4 +1,3 @@
-<!-- <?php var_dump(compact('google_analytics_id', 'google_ads_id')); ?> -->
 <?php if ( ! empty( $google_analytics_id ) ) : ?>
     <!-- Global Site Tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=<?php echo $google_analytics_id; ?>"></script>
