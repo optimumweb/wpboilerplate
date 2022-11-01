6a11b5f44f7bcf38f631031fa6ea23fade6ccad6
Jonathan Roy <jroy66@gmail.com>
2022-11-01 13:47:14 -0400

    make sure HTTP_HOST is set

--- inc/wpbp-security.php
+++ inc/wpbp-security.php
@@ -3,7 +3,7 @@
 function wpbp_force_https() {
     if ( isset($_SERVER['HTTP_HOST']) && ! is_ssl() && wpbp_get_option( 'force_https' ) === 'yes' ) {
         wp_redirect( 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'], 301 );
-        exit(
+        exit();
     }
 }
 add_action( 'template_redirect', 'wpbp_force_https', 1 );
